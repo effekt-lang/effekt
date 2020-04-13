172c7a7d54966e3822b10de36b663c3aa82bc015
Jonathan Brachthäuser <jonathan@b-studios.de>
2020-04-13 21:49:47 +0200

    Implement highlighter

---
+++ shared/src/main/scala/effekt/util/Highlight.scala
@@ -0,0 +1,68 @@
+package effekt
+package util
+
+import org.bitbucket.inkytonik.kiama.util.Positions
+
+import scala.annotation.tailrec
+import scala.util.matching._
+
+object Highlight {
+
+  object parser extends Parser(new Positions)
+
+  case class Token(r: Regex, highlight: String => String = identity) {
+    def apply(in: String): Option[(String, String)] =
+      r.findPrefixMatchOf(in) map {
+        res => (highlight(res.matched), in.substring(res.end))
+      }
+  }
+
+  val keyword = Token(
+    parser.keywordStrings.mkString("|").r,
+    Console.MAGENTA + _ + Console.RESET
+  )
+
+  val ident = Token(parser.name)
+
+  val comment = Token(
+    parser.singleline,
+    Console.WHITE + _ + Console.RESET
+  )
+
+  // TODO reuse number regex from parser here
+  val number = Token(
+    """[+-]?[0-9]+([.][0-9]+)?""".r,
+    Console.BLUE + _ + Console.RESET
+  )
+
+  val string = Token(
+    """["][^"]*["]""".r,
+    Console.GREEN + _ + Console.RESET
+  )
+
+  val whitespace = Token("""[\s]+""".r)
+
+  val any = Token(""".""".r)
+
+  val tokens = List(whitespace, comment, keyword, ident, number, string, any)
+
+  final def highlight(input: String, out: StringBuffer): StringBuffer = {
+
+    var in = input
+
+    while (in.nonEmpty) {
+      val (hl, rest) =
+        whitespace(in) orElse
+          comment(in) orElse
+          keyword(in) orElse
+          ident(in) orElse
+          number(in) orElse
+          string(in) orElse
+          any(in) getOrElse { sys error s"Cannot lex input ${in}" }
+
+      out.append(hl)
+      in = rest
+    }
+    out
+  }
+}
