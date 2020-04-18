790779d4a9340c2e210661d82ecdd583513f3fe1
Jonathan Brachthäuser <jonathan@b-studios.de>
2020-04-18 11:52:18 +0200

    Switch back to directly executing files in examples folder (by using MarkdownSource)

--- jvm/src/test/scala/effekt/RegressionTests.scala
+++ jvm/src/test/scala/effekt/RegressionTests.scala
@@ -12,15 +12,15 @@ import org.scalatest.funspec.AnyFunSpec
 import scala.language.implicitConversions
 
 class RegressionTests extends AnyFunSpec with TestUtils {
-  prepareTestsIn(examplesDir)
-  runTestsIn(targetDir)
+
+  runTestsIn(examplesDir)
 
   def runTestsIn(dir: File): Unit = describe(dir.getName) {
     dir.listFiles.foreach {
       case f if f.isDirectory => runTestsIn(f)
-      case f if f.getName.endsWith(".effekt") =>
+      case f if f.getName.endsWith(".effekt") || f.getName.endsWith(".md") =>
         val path = f.getParentFile
-        val baseName = f.getName.stripSuffix(".effekt")
+        val baseName = f.getName.stripSuffix(".md").stripSuffix(".effekt")
 
         val checkfile = path / (baseName + ".check")
 
@@ -45,41 +45,6 @@ trait TestUtils {
   // The sources of all testfiles are stored here:
   lazy val examplesDir = new File("examples")
 
-  // All testfiles are copied to this directory and .md files are converted to .effekt files
-  lazy val targetDir = new File("jvm") / "target" / "test"
-
-  lazy val fenceLine = """^```[^`\n]*$""".r
-
-  /**
-   * Generates .effekt files from .md files; copies all other files
-   */
-  def prepareTestsIn(dir: File): Unit = dir.listFiles.foreach {
-    case f if f.isDirectory             => prepareTestsIn(f)
-    case f if f.getName.endsWith(".md") => prepareTestsFor(f)
-    case f =>
-      val targetFile = targetDir / f.relativeTo(examplesDir).get.toString
-      IO.copyFile(f, targetFile)
-  }
-
-  /**
-   * Generates .effekt files from .md files; copies all other files
-   */
-  def prepareTestsFor(mdFile: File): Unit = {
-
-    var inCode = false
-
-    val lines = IO.readLines(mdFile).flatMap {
-      case line if fenceLine.matches(line) =>
-        inCode = !inCode
-        None
-      case line if inCode =>
-        Some(line)
-      case _ => None
-    }
-
-    IO.writeLines(targetFor(mdFile), lines)
-  }
-
   def interpret(file: File): String = {
     val compiler = new effekt.Driver {}
     val configs = compiler.createConfig(Seq("--Koutput", "string", "--lib", "lib"))
@@ -87,24 +52,6 @@ trait TestUtils {
     compiler.compileFile(file.getPath, configs)
     configs.stringEmitter.result().replaceAll("\u001B\\[[;\\d]*m", "")
   }
-
-  def targetFor(srcFile: File): File = {
-    val fileInTarget = targetDir / srcFile.relativeTo(examplesDir).getOrElse {
-      sys error s"Cannot compute relative path to examples directory for ${srcFile}"
-    }.toString
-
-    if (fileInTarget.name.endsWith(".md")) {
-      val baseName = fileInTarget.getPath.stripSuffix(".md")
-      new File(baseName + ".effekt")
-    } else {
-      fileInTarget
-    }
-  }
-
-  def sourceFor(targetFile: File): File =
-    examplesDir / targetFile.relativeTo(targetDir).getOrElse {
-      sys error s"Cannot compute relative path to target directory for ${targetFile}"
-    }.toString
 
   /**
    * Generates the check files from the actual outputs.
@@ -129,14 +76,13 @@ trait TestUtils {
 
         val out = interpret(f)
         // save checkfile in source folder (e.g. examples/)
-        IO.write(sourceFor(checkfile), out)
+        IO.write(checkfile, out)
       case _ => ()
     }
   }
 
   def generateCheckFiles(): Unit = {
-    prepareTestsIn(examplesDir)
-    generateCheckFilesIn(targetDir)
+    generateCheckFilesIn(examplesDir)
   }
 }
 object TestUtils extends TestUtils
