ccfe152549c2a1e8c599620dfba52208e2f3d4de
Philipp Schuster <philipp.schuster@uni-tuebingen.de>
2022-07-18 19:28:54 +0200

    Another test works.

--- effekt/shared/src/main/scala/effekt/machine/Analysis.scala
+++ effekt/shared/src/main/scala/effekt/machine/Analysis.scala
@@ -1,5 +1,20 @@
 package effekt.machine
 
 def freeVariables(clause: Clause): Set[Variable] =
-  Set()
+  clause match {
+    case Clause(parameters, body) => freeVariables(body) -- parameters.toSet
+  }
 
+def freeVariables(statement: Statement): Set[Variable] =
+  statement match {
+    case Jump(Label(_, environment)) =>
+      environment.toSet
+    case Substitute(bindings, rest) =>
+      freeVariables(rest) -- bindings.map(_._1).toSet ++ bindings.map(_._2).toSet
+    case PushFrame(frame, rest) =>
+      freeVariables(frame) ++ freeVariables(rest)
+    case Return(environment) =>
+      environment.toSet
+    case Run(_, environment, continuation) =>
+          environment.toSet ++ continuation.flatMap(freeVariables)
+  }
