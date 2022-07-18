4f1c0f6bf9193fc39648f7ccdb40b7acccc83732
Jonathan Frech <info@jfrech.com>
2022-07-18 12:58:36 +0200

    compilation plumbing (hinging on the existence of `LLVMFragmentPrinter.wholeProgramOneshot`)

--- effekt/shared/src/main/scala/effekt/generator/LLVM.scala
+++ effekt/shared/src/main/scala/effekt/generator/LLVM.scala
@@ -1,6 +1,8 @@
 package effekt
 package generator
 
+import effekt.llvm.{ LLVMFragmentPrinter }
+
 import kiama.output.PrettyPrinterTypes.{ Document, emptyLinks }
 import kiama.util.{ Source, Counter }
 
@@ -15,37 +17,39 @@ import scala.language.implicitConversions
 import effekt.util.paths._
 
 object LLVM extends Backend {
-  def compileWhole(main: CoreTransformed, dependencies: List[CoreTransformed])(implicit C: Context): Option[Compiled] = ???
-  def compileSeparate(input: CoreTransformed)(implicit C: Context): Option[Document] = ???
+  def compileWhole(main: CoreTransformed, dependencies: List[CoreTransformed])(implicit C: Context): Option[Compiled] = {
+    if (dependencies.length > 0)
+      C.abort("llvm does not support dependencies (yet?)")
+
+    val doc = run(C.module.source).get
+
+    val p = path(main.mod)
+    Some(Compiled(p, Map(p -> doc)))
+  }
 
   def path(m: Module)(implicit C: Context): String =
     (C.config.outputPath() / m.path.replace('/', '_')).unixPath
 
-  /**
-   * This is only called on the main entry point, we have to manually traverse the dependencies
-   * and write them.
-   */
-  //TODO-LLVM refactor
-  def run(src: Source)(implicit C: Context): Option[Document] = {
+  private def run(src: Source)(implicit C: Context): Option[Document] = {
     None
-    // val modQ = C.frontend(src)
-    // if (modQ.isEmpty)
-    //   return None
-    // val mod = modQ.get
+    val mod = C.frontend(src).get
 
-    // val mainName = C.checkMain(mod)
+    val mainName = C.checkMain(mod)
 
-    // // TODO why is backend returning Option? What if compilation fails?
-    // val coreMods = (mod.dependencies :+ mod).flatMap(m => C.backend(m.source))
-    // // TODO get rid of this object!
-    // val machiner = new machine.Transformer
-    // // TODO get rid of the module! we only need it for fresh name generation
-    // val llvmDefs = C.using(module = mod) {
-    //   val machineMods = coreMods.map(m => machiner.transform(m)(machiner.TransformerContext(C)))
-    //   machineMods.flatMap(m => LLVMTransformer.transform(m))
-    // }
+    // TODO why is backend returning Option? What if compilation fails?
+    val coreMods = (mod.dependencies :+ mod).flatMap(m => C.backend(m.source))
+    // TODO get rid of this object!
+    val machiner = new machine.Transformer
+    // TODO get rid of the module! we only need it for fresh name generation
+    val llvmDefs = C.using(module = mod) {
+      val machineMods = coreMods.map(m => machiner.transform(m)(machiner.TransformerContext(C)))
+      machineMods.flatMap(m => LLVMTransformer.transform(m))
+    }
 
-    // val llvm: LLVMFragment = LLVMFragmentPrinter.wholeProgramOneshot(mainName, llvmDefs)
-    // return Some(Document(llvm, emptyLinks))
+    val llvm: LLVMFragment = LLVMFragmentPrinter.wholeProgramOneshot(mainName, llvmDefs)
+    return Some(Document(llvm, emptyLinks))
   }
+
+  // apparently only used by the LSP; currently unsupported
+  def compileSeparate(input: CoreTransformed)(implicit C: Context): Option[Document] = ???
 }
