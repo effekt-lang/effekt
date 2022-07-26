f72a1aad35158de8ae77055d661eb46e2d9ea274
Jonathan Frech <info@jfrech.com>
2022-07-26 16:48:39 +0200

    cleaned up test directory, added passing test `minus-one.effekt`

--- effekt/jvm/src/test/scala/effekt/LLVMTests.scala
+++ effekt/jvm/src/test/scala/effekt/LLVMTests.scala
@@ -9,17 +9,20 @@ import scala.sys.process.Process
 
 class LLVMTests extends AnyFunSpec {
   Process(Seq("rm", "-rf", "out/")).!! // TODO globally configure sbt to clear out before running tests
-  go(new File("tests-llvm"))
+  findAllTests(new File("tests/llvm"))
 
-  def go(root: File): Unit = describe(root.getName) {
+  // Unix jargon for odd Scala names:
+  // `.getName` -> "base"
+  // `getParentFile` -> "dir"
+  def findAllTests(root: File): Unit = describe(root.getName) {
     root.listFiles.foreach {
       case h if h.getName.startsWith(".") => ()
-      case d if d.isDirectory             => go(d)
+      case d if d.isDirectory             => findAllTests(d)
       case e if e.getName.endsWith(".effekt") =>
-        val dirname = e.getParentFile // TODO ugly
+        val dirname = e.getParentFile
         val c = dirname / (e.getName.stripSuffix(".effekt") + ".check")
         if (!c.exists())
-          sys error s"missing checkfile: ${e.getPath}"
+          sys error s"missing checkfile for: ${e.getPath}"
         it(e.getName) {
           assert(IO.read(c).toString == run(e))
         }
@@ -40,4 +43,3 @@ class LLVMTests extends AnyFunSpec {
     configs.stringEmitter.result().replaceAll("\u001B\\[[;\\d]*m", "") // XXX this is a hack
   }
 }
-
