38c9e6bcbca507ede527ddcbe0afa5447eeda3fe
Jonathan Brachthäuser <jonathan@b-studios.de>
2022-08-30 13:02:00 +0200

    Convert llvm.Tree to Scala 3 enums

--- effekt/shared/src/main/scala/effekt/generator/llvm/Tree.scala
+++ effekt/shared/src/main/scala/effekt/generator/llvm/Tree.scala
@@ -4,50 +4,63 @@ package llvm
 /**
  *  see: https://hackage.haskell.org/package/llvm-hs-pure-9.0.0/docs/LLVM-AST.html#t:Definition
  */
-sealed trait Definition
-case class Function(returnType: Type, name: String, parameters: List[Parameter], basicBlocks: List[BasicBlock]) extends Definition
-case class VerbatimFunction(returnType: Type, name: String, parameters: List[Parameter], body: String) extends Definition
-case class Verbatim(content: String) extends Definition
+enum Definition {
+  case Function(returnType: Type, name: String, parameters: List[Parameter], basicBlocks: List[BasicBlock])
+  case VerbatimFunction(returnType: Type, name: String, parameters: List[Parameter], body: String)
+  case Verbatim(content: String)
+}
+export Definition.*
 
 case class BasicBlock(name: String, instructions: List[Instruction], terminator: Terminator)
 
 /**
  *  see: https://hackage.haskell.org/package/llvm-hs-pure-9.0.0/docs/LLVM-AST-Instruction.html#t:Instruction
  */
-sealed trait Instruction
-case class Call(result: String, resultType: Type, function: Operand, arguments: List[Operand]) extends Instruction
-case class TailCall(function: Operand, arguments: List[Operand]) extends Instruction
-case class Load(result: String, address: Operand) extends Instruction
-case class Store(address: Operand, value: Operand) extends Instruction
-case class GetElementPtr(result: String, address: Operand, indices: List[Int]) extends Instruction
-case class BitCast(result: String, operand: Operand, typ: Type) extends Instruction
-case class Add(result: String, operand0: Operand, operand1: Operand) extends Instruction
-case class InsertValue(result: String, aggregate: Operand, element: Operand, index: Int) extends Instruction
-case class ExtractValue(result: String, aggregate: Operand, index: Int) extends Instruction
+enum Instruction {
+  case Call(result: String, resultType: Type, function: Operand, arguments: List[Operand])
+  case TailCall(function: Operand, arguments: List[Operand])
+  case Load(result: String, address: Operand)
+  case Store(address: Operand, value: Operand)
+  case GetElementPtr(result: String, address: Operand, indices: List[Int])
+  case BitCast(result: String, operand: Operand, typ: Type)
+  case Add(result: String, operand0: Operand, operand1: Operand)
+  case InsertValue(result: String, aggregate: Operand, element: Operand, index: Int)
+  case ExtractValue(result: String, aggregate: Operand, index: Int)
+}
+export Instruction.*
 
-sealed trait Terminator
-case class RetVoid() extends Terminator
-case class Switch(operand: Operand, defaultDest: String, dests: List[(Int, String)]) extends Terminator
-case class CondBr(condition: Operand, trueDest: String, falseDest: String) extends Terminator
+enum Terminator {
+  case RetVoid()
+  case Switch(operand: Operand, defaultDest: String, dests: List[(Int, String)])
+  case CondBr(condition: Operand, trueDest: String, falseDest: String)
+}
+export Terminator.*
 
 case class Parameter(typ: Type, name: String)
 
+// Operands cannot be an enum since we use the more specific types massively. Scala 3 will perform widening way
+// too often.
 sealed trait Operand
-case class LocalReference(tpe: Type, name: String) extends Operand
-case class ConstantInt(n: Int) extends Operand
-case class ConstantAggregateZero(typ: Type) extends Operand
-case class ConstantGlobal(tpe: Type, name: String) extends Operand
-case class ConstantNull(tpe: Type) extends Operand
+object Operand {
+  case class LocalReference(tpe: Type, name: String) extends Operand
+  case class ConstantInt(n: Int) extends Operand
+  case class ConstantAggregateZero(typ: Type) extends Operand
+  case class ConstantGlobal(tpe: Type, name: String) extends Operand
+  case class ConstantNull(tpe: Type) extends Operand
+}
+export Operand.*
 
 /**
  *  see: https://hackage.haskell.org/package/llvm-hs-pure-9.0.0/docs/LLVM-AST.html#t:Type
  */
-sealed trait Type
-case class VoidType() extends Type
-case class IntegerType64() extends Type
-case class IntegerType8() extends Type
-case class IntegerType1() extends Type
-case class StructureType(elementTypes: List[Type]) extends Type
-case class FunctionType(resultType: Type, argumentTypes: List[Type]) extends Type
-case class PointerType(pointerReferent: Type) extends Type
-case class NamedType(name: String) extends Type
+enum Type {
+  case VoidType()
+  case IntegerType64()
+  case IntegerType8()
+  case IntegerType1()
+  case StructureType(elementTypes: List[Type])
+  case FunctionType(resultType: Type, argumentTypes: List[Type])
+  case PointerType(pointerReferent: Type)
+  case NamedType(name: String)
+}
+export Type.*
