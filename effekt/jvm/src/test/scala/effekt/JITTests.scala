0ba31b545d47e43590f9bb928004b5d4c56a9eea
Marcial Gaißert <gaisseml@studi.informatik.uni-stuttgart.de>
2022-09-19 15:23:43 +0200

    JIT: Make JITTests consistent with the other ones

--- effekt/jvm/src/test/scala/effekt/JITTests.scala
+++ effekt/jvm/src/test/scala/effekt/JITTests.scala
@@ -8,30 +8,23 @@ import java.io.File
 import scala.language.implicitConversions
 import scala.sys.process.Process
 
-class JITTests extends AnyFunSpec {
-  Process(Seq("rm", "-rf", "out/")).!! // TODO globally configure sbt to clear out before running tests
-  findAllTests(new File("examples/pos/jit/"))
+class JITTests extends EffektTests {
 
-  // Unix jargon for odd Scala names:
-  // `.getName` -> "base"
-  // `getParentFile` -> "dir"
-  def findAllTests(root: File): Unit = describe(root.getName) {
-    root.listFiles.foreach {
-      case h if h.getName.startsWith(".") => ()
-      case d if d.isDirectory             => findAllTests(d)
-      case e if e.getName.endsWith(".effekt") =>
-        val dirname = e.getParentFile
-        val c = dirname / (e.getName.stripSuffix(".effekt") + ".check")
-        if (!c.exists())
-          sys error s"missing checkfile for: ${e.getPath}"
-        it(e.getName) {
-          assert(IO.read(c).toString == run(e))
-        }
-      case _ => ()
+  override lazy val included: List[File] = List(examplesDir / "pos" / "jit")
+
+  override lazy val ignored: List[File] = List(
+    // computes the wrong results
+    examplesDir / "pos" / "jit" / "emit.effekt"
+  )
+
+
+  def runTestFor(f: File, expected: String) =
+    it(f.getName + " (jit)") {
+      val out = runJIT(f)
+      assert(expected == out)
     }
-  }
 
-  def run(f: File): String = {
+  def runJIT(f: File): String = {
     // TODO flaky body
     val compiler = new effekt.Driver {}
     val configs = compiler.createConfig(Seq(
@@ -41,6 +34,6 @@ class JITTests extends AnyFunSpec {
     ))
     configs.verify()
     compiler.compileFile(f.getPath, configs)
-    configs.stringEmitter.result().replaceAll("\u001B\\[[;\\d]*m", "") // XXX this is a hack
+    removeAnsiColors(configs.stringEmitter.result())
   }
 }
