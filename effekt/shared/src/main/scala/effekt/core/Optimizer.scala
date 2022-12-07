5395a8effd861108946b83a9c16f5c1bfc5b58b8
Marcial Gaißert <uni@gaisseml.de>
2022-12-07 14:02:01 +0100

    Update `Optimizer` for new core.
    Fixes the compilation errors introduced by cherry-picked
    aa8b5cb2a913e3434dfd5f027f940231018e34cb.

--- effekt/shared/src/main/scala/effekt/core/Optimizer.scala
+++ effekt/shared/src/main/scala/effekt/core/Optimizer.scala
@@ -13,30 +13,30 @@ object Optimizer extends Phase[CoreTransformed, CoreTransformed] {
 
   def run(input: CoreTransformed)(using Context): Option[CoreTransformed] =
     input match {
-      case CoreTransformed(source, tree, mod, ModuleDecl(path, imports, defs, exports)) =>
-        val optimized = optimize(defs)
-        Some(CoreTransformed(source, tree, mod, ModuleDecl(path, imports, optimized, exports)))
+      case CoreTransformed(source, tree, mod, ModuleDecl(path, imports, decls, externs, defs, exports)) =>
+        val optimized = defs map optimize
+        Some(CoreTransformed(source, tree, mod, ModuleDecl(path, imports, decls, externs, optimized, exports)))
     }
 
-  def optimize(s: Stmt)(using Context): Stmt = {
+  def optimize(d: Definition)(using Context): Definition = {
 
     // a very small and easy post processing step...
     // reduces run-return pairs
     object eliminateReturnRun extends core.Tree.Rewrite {
       override def expr = {
-        case core.Run(core.Return(p), _) => rewrite(p)
+        case core.Run(core.Return(p)) => rewrite(p)
       }
     }
 
     // rewrite (Val (Return e) s) to (Let e s)
     object directStyleVal extends core.Tree.Rewrite {
       override def stmt = {
-        case core.Val(id, tpe, core.Return(expr), body) =>
-          core.Let(id, tpe, rewrite(expr), rewrite(body))
+        case core.Val(id, core.Return(expr), body) =>
+          core.Let(id, rewrite(expr), rewrite(body))
       }
     }
 
-    val opt = eliminateReturnRun.rewrite(s)
+    val opt = eliminateReturnRun.rewrite(d)
     val res = directStyleVal.rewrite(opt)
 
     // More optimizations can go here.
