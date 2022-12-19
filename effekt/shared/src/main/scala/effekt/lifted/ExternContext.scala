70583f8f18f7074f4380d30c6a0ed726fbda5755
Marcial Gaißert <uni@gaisseml.de>
2022-12-19 19:38:38 +0100

    Use names Int/String/Double/Boolean in core parser
    instead of fixed builtins

---
+++ effekt/shared/src/main/scala/effekt/lifted/ExternContext.scala
@@ -0,0 +1,16 @@
+package effekt.core
+
+import effekt.util.messages.ErrorReporter
+
+class ExternContext(val externs: List[Extern]) {
+
+  lazy val types = externs.collect{
+    case t: Extern.Type => (t.id -> t)
+  }.toMap
+
+  def findExternType(id: Id): Option[Extern.Type] = types.get(id)
+  def getExternType(id: Id)(using E: ErrorReporter): Extern.Type = findExternType(id).getOrElse{
+    E.panic(s"Cannot find extern type declaration for ${id}")
+  }
+
+}
