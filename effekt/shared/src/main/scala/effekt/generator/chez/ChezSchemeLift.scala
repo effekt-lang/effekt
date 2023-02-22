37300bba3fac31a17d3a53ef1e806bb31aab6535
Jonathan Brachthäuser <jonathan@b-studios.de>
2023-02-22 10:33:45 +0100

    Slight cleanup of ML transformer

--- effekt/shared/src/main/scala/effekt/generator/chez/ChezSchemeLift.scala
+++ effekt/shared/src/main/scala/effekt/generator/chez/ChezSchemeLift.scala
@@ -4,7 +4,6 @@ package chez
 
 import effekt.context.Context
 import effekt.lifted.LiftInference
-
 import kiama.output.PrettyPrinterTypes.Document
 import kiama.util.Source
 
@@ -16,17 +15,17 @@ class ChezSchemeLift extends Compiler[String] {
   def extension = ".ss"
 
   override def prettyIR(source: Source, stage: Stage)(using Context): Option[Document] = stage match {
-    case Stage.Core => Core(source).map { res => core.PrettyPrinter.format(res.core) }
-    case Stage.Lifted => (Core andThen LiftInference)(source).map { res => lifted.PrettyPrinter.format(res.core) }
+    case Stage.Core => steps.afterCore(source).map { res => core.PrettyPrinter.format(res.core) }
+    case Stage.Lifted => steps.afterLift(source).map { res => lifted.PrettyPrinter.format(res.core) }
     case Stage.Machine => None
-    case Stage.Target => Separate(source).map { expr => pretty(expr) }
+    case Stage.Target => steps.afterChez(source).map { expr => pretty(expr) }
   }
 
   override def treeIR(source: Source, stage: Stage)(using Context): Option[Any] = stage match {
-    case Stage.Core => Core(source).map { res => res.core }
-    case Stage.Lifted => (Core andThen LiftInference)(source).map { res => res.core }
+    case Stage.Core => steps.afterCore(source).map { res => res.core }
+    case Stage.Lifted => steps.afterLift(source).map { res => res.core }
     case Stage.Machine => None
-    case Stage.Target => Separate(source)
+    case Stage.Target => steps.afterChez(source)
   }
 
   override def compile(source: Source)(using C: Context) = Compile(source)
@@ -36,13 +35,13 @@ class ChezSchemeLift extends Compiler[String] {
   // ------------------------
   // Source => Core => Lifted => Chez
   lazy val Compile =
-    allToCore(Core) andThen Aggregate andThen LiftInference andThen Chez map { case (main, expr) =>
+    allToCore(Core) andThen Aggregate andThen LiftInference andThen ToChez map { case (main, expr) =>
       (Map(main -> pretty(expr)), main)
     }
 
   lazy val Core = Phase.cached("core") { Frontend andThen Middleend }
 
-  lazy val Chez = Phase("chez") {
+  lazy val ToChez = Phase("chez") {
     case CoreLifted(source, tree, mod, core) =>
       val mainSymbol = Context.checkMain(mod)
       val mainFile = path(mod)
@@ -51,9 +50,14 @@ class ChezSchemeLift extends Compiler[String] {
 
   // The Compilation Pipeline for VSCode
   // -----------------------------------
-  lazy val Separate =
-    allToCore(Core) map { all => all.main } andThen LiftInference andThen Chez map { case (_, expr) => expr }
-
+    // The Compilation Pipeline for VSCode
+  // -----------------------------------
+  object steps {
+    // intermediate steps for VSCode
+    val afterCore = allToCore(Core) map { c => c.main }
+    val afterLift = afterCore andThen LiftInference
+    val afterChez = afterLift andThen ToChez map { case (f, prog) => prog }
+  }
 
   // Helpers
   // -------
