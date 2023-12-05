f80efd0d2cefe86a886105c47493cbc30e9a2bb1
Jonathan Brachthäuser <jonathan@b-studios.de>
2023-12-05 13:55:00 +0100

    Also rename in tests

--- effekt/jvm/src/test/scala/effekt/core/OptimizerTests.scala
+++ effekt/jvm/src/test/scala/effekt/core/OptimizerTests.scala
@@ -32,13 +32,13 @@ class OptimizerTests extends CoreTests {
 
   def inlineOnce(input: String, expected: String)(using munit.Location) =
     assertTransformsTo(input, expected) { tree =>
-      val (result, count) = InlineUnique.once(Set(mainSymbol), tree)
+      val (result, count) = Inline.once(Set(mainSymbol), tree)
       result
     }
 
   def inlineFull(input: String, expected: String)(using munit.Location) =
     assertTransformsTo(input, expected) { tree =>
-      InlineUnique.full(Set(mainSymbol), tree)
+      Inline.full(Set(mainSymbol), tree)
     }
 
   test("toplevel"){
