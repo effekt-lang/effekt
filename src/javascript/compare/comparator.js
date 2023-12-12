5a4a8c84193b21de43dd63d98dec480cca84b6ca
IR0NSIGHT <irnsght@googlemail.com>
2023-12-12 16:47:23 +0100

    migrate effekt benchmarks to use cmdArgs and autoRunner to supply them. return type of benchmarks is List[Int] now instead of sum of all executions

--- src/javascript/compare/comparator.js
+++ src/javascript/compare/comparator.js
@@ -23,26 +23,44 @@ const execute = (command, onOutput) => {
 function executeCommands(commands, isVerify) {
   const outputs = [];
   commands.forEach((command, index) => {
-    const performance = { name: command[0], effekt: -1, js: -1 }
+    const performance = { name: command[0], effekt: {}, js: {} }
     outputs.push(performance)
+
     console.log("running benchmark:", performance.name)
     const dirtyCd = "cd " + __dirname + " && cd ../../.. && "
+    const amount = " 3 "
     const verifyArgs = isVerify ? " --verify" : ""
 
     const [executableName, effektPath, jsCmd ] = command
-    const effektCmd = `effekt.sh -b ${effektPath} && ./out/${executableName} ` 
-    execute(dirtyCd + effektCmd + verifyArgs, (time) => performance.effekt = time)
+    const effektCmd = dirtyCd+`effekt.sh -b ${effektPath} && ./out/${executableName} `+amount+verifyArgs 
+    console.log("run: ", effektCmd)
+    execute(effektCmd, (time) => performance.effekt = time)
 
-
-    execute(dirtyCd + jsCmd + verifyArgs, (time) => performance.js = time)
+    // run pure JS benchmark 
+    execute(dirtyCd + jsCmd + amount + verifyArgs, (time) => performance.js = `[${parseFloat(time)}]`)
   });
 
+  const analysis = outputs.map(mark => ({...mark, effekt: analyzeDurations(mark.effekt), js: analyzeDurations(mark.js)}))
+
   const outputFile = "fasteffekt_results.json"
-  const resultString = JSON.stringify(outputs.map(perf => ({ ...perf, ratio: perf.effekt / perf.js })), null, 3)
+  const resultString = JSON.stringify(analysis.map(perf => ({ ...perf, ratio: perf.effekt.sum / perf.js.sum })), null, 3)
   fs.writeFileSync(outputFile, resultString);
   console.log(`Command outputs saved to ${outputFile}`);
-  console.log(resultString)
+  console.log(analysis.map( mark => ({ name: mark.name, effekt: mark.effekt.sum, js: mark.js.sum, ratio: mark.effekt.sum/mark.js.sum })))
 }
+
+/**
+ * analyze duration array
+ * @param {string} durations : list of duration times for every benchmark run
+ */
+const analyzeDurations = (durations) => {
+  durations = JSON.parse(durations);
+  const sum = durations.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  const avg = sum/durations.length
+
+  return { sum: sum, avg: avg, durations: durations }
+}
+
 
 const runAll = (isVerify) => executeCommands(commands, isVerify)
 module.exports = runAll
