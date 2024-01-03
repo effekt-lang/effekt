e5647af3e85d05d01294fc4eb7584faea555a853
IR0NSIGHT <irnsght@googlemail.com>
2024-01-03 16:50:19 +0100

    improved CLI documentation and options

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,9 +1,31 @@
 #!/usr/bin/env node
 
-console.log("Running fasteffekt benchmarks (this may take a couple minutes)")
+const { verify } = require('crypto');
 const path = require('path');
 const runAll = require(path.resolve(__dirname, "./compare/comparator"))
 
-const isVerify = (process.argv.length>2 && process.argv[2].toLowerCase() == "--verify")
-console.log("is verify-mode:",isVerify)
-runAll(isVerify)
+const arg = process.argv.length > 2 ? process.argv[2].toLowerCase() : ""
+const isHelp = (arg == "--help" || arg == "-h")
+
+if (isHelp) {
+  console.log(`
+  fasteffekt - by Maximilian Marschall
+  benchmarking the current install of the effekt language.
+  will execute benchmarks in effekt and JS.
+  outputs results to console and JSON file
+  options:
+    --help, -h:      documentation
+    --small, -s:     run minimal benchmark to verify they all work
+    --version, -v:   show version of fasteffekt
+  `)
+} else if (arg == "--version" || arg == "-v") {
+  const packageJson = require('../../package.json');
+  // Access the version field and log it
+  console.log('Package version:', packageJson.version);
+} else {
+  const isVerify = (arg == "--small" || arg == "-s")
+  if (isVerify)
+    console.log("verify-mode:", isVerify)
+  runAll(isVerify ? "--verify" : "")
+}
+
