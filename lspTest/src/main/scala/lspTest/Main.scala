5fd066916a24bcd30eeb7edc60cf93ab80e7b6a4
Marvin Borner <git@marvinborner.de>
2024-01-04 21:10:26 +0100

    Add child spawning with repeated connection attempts

--- lspTest/src/main/scala/lspTest/Main.scala
+++ lspTest/src/main/scala/lspTest/Main.scala
@@ -4,13 +4,16 @@ import scala.scalajs.js
 import scala.concurrent.ExecutionContext
 import scala.scalajs.concurrent.JSExecutionContext
 import scala.util.{Success, Failure}
-import typings.node.netMod.createConnection
+import typings.node.childProcessMod
+import typings.node.netMod.NetConnectOpts
+import typings.node.netMod.{NetConnectOpts, createConnection}
+import scala.scalajs.js.timers
 import typings.vscodeJsonrpc.libCommonConnectionMod.Logger
 import typings.vscodeJsonrpc.libCommonMessageReaderMod.MessageReader
 import typings.vscodeJsonrpc.libCommonMessageWriterMod.MessageWriter
-import typings.vscodeLanguageserverProtocol.libCommonConnectionMod.createProtocolConnection
 import typings.vscodeLanguageserverProtocol.libCommonProtocolMod.InitializeParams
 import typings.vscodeLanguageserverProtocol.mod.InitializeRequest
+import typings.vscodeLanguageserverProtocol.libCommonConnectionMod
 
 object Main {
   def test(client: Client) {
@@ -21,21 +24,34 @@ object Main {
     }
   }
 
+  def tryConnect(port: Int, host: String) {
+    val socket = createConnection(port, host)
+
+    // needed since server process doesn't start immediately
+    socket.on("error", ev => {
+      timers.setTimeout(500) {
+        tryConnect(port, host)
+      }
+    })
+
+    socket.on("connect", ev => {
+      val reader = socket.asInstanceOf[MessageReader]
+      val writer = socket.asInstanceOf[MessageWriter]
+      val logger = Logger(println, println, println, println)
+      val connection = libCommonConnectionMod.createProtocolConnection(reader, writer, logger)
+      connection.listen()
+
+      implicit val ec: ExecutionContext = JSExecutionContext.queue
+      connection.sendRequest(InitializeRequest.method.asInstanceOf[String], InitializeParams).toFuture.onComplete {
+        case Success(_) => test(new Client(connection))
+        case Failure(_) => println("error during initialization")
+      }
+    })
+  }
+
   def main(args: Array[String]): Unit = {
-    // val process = typings.node.childProcessMod.spawn("effekt.sh", js.Array("-s", "--debug"))
-    // TODO: createConnection with opts and TCP timeout
+    val process = childProcessMod.spawn("effekt.sh", js.Array("-s", "--debug"))
 
-    val socket = createConnection(5007, "127.0.0.1")
-    val reader = socket.asInstanceOf[MessageReader]
-    val writer = socket.asInstanceOf[MessageWriter]
-    val logger = Logger(println, println, println, println)
-    val connection = createProtocolConnection(reader, writer, logger)
-    connection.listen()
-
-    implicit val ec: ExecutionContext = JSExecutionContext.queue
-    connection.sendRequest(InitializeRequest.method.asInstanceOf[String], InitializeParams).toFuture.onComplete {
-      case Success(_) => test(new Client(connection))
-      case Failure(_) => println("error during initialization")
-    }
+    tryConnect(5007, "127.0.0.1")
   }
 }
