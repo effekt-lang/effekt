aab6f11677a5668a8e0cd5c0c7150fafb3947ecb
Marvin Borner <git@marvinborner.de>
2024-01-18 16:17:53 +0100

    Better solution for sequential execution of future tests

---
+++ lspTest/src/test/scala/lspTest/SequentialExecutor.scala
@@ -0,0 +1,20 @@
+package lspTest
+
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import utest._
+
+// utest runs async tests in *parallel*, while we want sequential execution
+// This implements a utest wrapper that chains all futures returned from tests
+
+object SequentialExecutor extends SequentialExecutor
+
+trait SequentialExecutor extends framework.Executor {
+  implicit val ec: ExecutionContext = ExecutionContext.global
+
+  var futureChain: Future[Any] = Future { "root" }
+
+  override def utestWrap(path: Seq[String], runBody: => Future[Any])(implicit ec: ExecutionContext): Future[Any] = {
+    futureChain = futureChain.flatMap { _ => runBody }
+    futureChain
+  }
+}
