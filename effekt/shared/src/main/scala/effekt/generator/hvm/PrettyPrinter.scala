3ff72aa02af255a962a80363d3fc4c97693248d1
lseif <lars@seifcity.de>
2024-03-13 12:20:06 +0100

    added hvm pretty printer

--- effekt/shared/src/main/scala/effekt/generator/hvm/PrettyPrinter.scala
+++ effekt/shared/src/main/scala/effekt/generator/hvm/PrettyPrinter.scala
@@ -1,2 +1,28 @@
-//package effekt.generator.hvm
+package effekt
+package generator
+package hvm
+
+import effekt.util.intercalate
+import kiama.output.ParenPrettyPrinter
+import kiama.output.PrettyPrinterTypes.Document
+
+import scala.language.implicitConversions
+
+object PrettyPrinter extends ParenPrettyPrinter {
+
+  override val defaultIndent = 2
+
+  val prelude = "#!/usr/local/bin/hvm --script\n\n(import (hvm))\n\n"
+
+  //def toDoc(name: ChezName): Doc = name.name
+
+  //def format(defs: List[Def]): Document =
+    //pretty(vsep(defs map toDoc, line <> line))
+
+  //def toDoc(binding: Binding): Doc = brackets(toDoc(binding.name) <+> toDoc(binding.expr))
+
+  def toDoc(expr: Term): Doc = String("IT WORKS!!!!!")
+  }
+
+ 
 
