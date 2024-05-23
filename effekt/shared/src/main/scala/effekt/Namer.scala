f0fa7e253bd80b3a81b6681245ccf82fc4fd0f79
Jonathan Brachthäuser <jonathan@b-studios.de>
2024-05-23 19:44:24 +0200

    Drop commented out code

--- effekt/shared/src/main/scala/effekt/Namer.scala
+++ effekt/shared/src/main/scala/effekt/Namer.scala
@@ -303,17 +303,6 @@ object Namer extends Phase[Parsed, NameResolved] {
       }
 
     case f @ source.ExternDef(capture, id, tparams, vparams, bparams, ret, bodies) =>
-    //      if (!bodies.supportedByFeatureFlags(Context.compiler.supportedFeatureFlags)) {
-    //        val featureFlags = bodies.map(_.featureFlag)
-    //        //        Context.warning(pp"Extern definition ${id} is not supported as it is only defined for feature flags ${featureFlags.mkString(", ")}," +
-    //        //          pp"but the current backend only supports ${Context.compiler.supportedFeatureFlags.mkString(", ")}.")
-    //      }
-    //      bodies.foreach {
-    //        case source.ExternBody.StringExternBody(ff, _) if ff.isDefault =>
-    //          Context.warning(pp"Extern definition ${id} contains extern string without feature flag. This will likely not work in other backends, "
-    //            + pp"please annotate it with a feature flag (Supported by the current backend: ${Context.compiler.supportedFeatureFlags.mkString(", ")})")
-    //        case _ => ()
-    //      }
       val sym = f.symbol
       Context scoped {
         sym.tparams.foreach { p => Context.bind(p) }
