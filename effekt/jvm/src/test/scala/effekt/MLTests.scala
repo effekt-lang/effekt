1aab39af46c8d39678b49330467cff831d47956a
Marcial Gaißert <uni@gaisseml.de>
2024-06-13 13:07:21 +0200

    Re-enable test for ML - equals exists there, too

--- effekt/jvm/src/test/scala/effekt/MLTests.scala
+++ effekt/jvm/src/test/scala/effekt/MLTests.scala
@@ -124,6 +124,5 @@ class MLTests extends EffektTests {
     examplesDir / "pos" / "probabilistic.effekt",
 
     examplesDir / "pos" / "genericcompare.effekt", // genericCompare is only implemented for JS
-    examplesDir / "pos" / "issue429.effekt",       //
   )
 }
