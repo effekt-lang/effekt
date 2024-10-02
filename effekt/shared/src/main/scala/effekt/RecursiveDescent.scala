18545b771934f29525614aae07da8f3887ec96ef
Jiří Beneš <mail@jiribenes.com>
2024-10-02 14:52:42 +0200

    Try reporting a better message on malformed operation implementation

--- effekt/shared/src/main/scala/effekt/RecursiveDescent.scala
+++ effekt/shared/src/main/scala/effekt/RecursiveDescent.scala
@@ -15,10 +15,13 @@ import scala.util.boundary.break
 
 
 case class Fail(message: String, position: Int) extends Throwable(null, null, false, false)
+case class SoftFail(message: String, positionStart: Int, positionEnd: Int)
 
 class RecursiveDescent(positions: Positions, tokens: Seq[Token], source: Source) {
 
   import scala.collection.mutable.ListBuffer
+
+  val softFails: ListBuffer[SoftFail] = ListBuffer[SoftFail]()
 
   def parse(input: Input)(using C: Context): Option[ModuleDecl] =
 
@@ -28,6 +31,16 @@ class RecursiveDescent(positions: Positions, tokens: Seq[Token], source: Source)
       val res = Some(program())
       //val after = System.currentTimeMillis()
       //println(s"${input.source.name}: ${after - before}ms")
+
+      // Report soft fails
+      softFails.foreach {
+        case SoftFail(msg, from, to) =>
+          val source = input.source
+          val fromPos = source.offsetToPosition(tokens(from).start)
+          val toPos = source.offsetToPosition(tokens(to).end) // NOTE(jiribenes): I think this is correct, we don't need the +1, right?
+          val range = Range(fromPos, toPos)
+          C.report(effekt.util.messages.ParseError(msg, Some(range)))
+      }
 
       res
     } catch {
@@ -711,6 +724,24 @@ class RecursiveDescent(positions: Positions, tokens: Seq[Token], source: Source)
     nonterminal:
       (`def` ~> idRef()) ~ paramsOpt() ~ maybeReturnAnnotation() ~ (`=` ~> stmt()) match {
         case id ~ (tps, vps, bps) ~ ret ~ body =>
+         if (isSemi) {
+           semi()
+
+           val startPosition = position
+
+           // TODO(jiribenes): would be nice to abstract this and have a `consumeUntil` function
+           if (!peek(`}`) && !peek(`def`) && !peek(EOF)) {
+              // consume until the next `def` or `}` or EOF
+              while (!peek(`}`) && !peek(`def`) && !peek(EOF)) {
+                next()
+              }
+
+              val endPosition = position
+              val msg = "Unexpected tokens after operation definition. Expected either a new operation definition or the end of the implementation."
+              softFail(msg, startPosition, endPosition)
+            }
+          }
+
           // TODO the implicitResume needs to have the correct position assigned (maybe move it up again...)
           OpClause(id, tps, vps, bps, ret, body, implicitResume)
       }
@@ -1213,6 +1244,10 @@ class RecursiveDescent(positions: Positions, tokens: Seq[Token], source: Source)
    * Aborts parsing with the given message
    */
   def fail(message: String): Nothing = throw Fail(message, position)
+
+  def softFail(message: String, start: Int, end: Int): Unit = {
+    softFails += SoftFail(message, start, end)
+  }
 
   /**
    * Guards `thn` by token `t` and consumes the token itself, if present.
