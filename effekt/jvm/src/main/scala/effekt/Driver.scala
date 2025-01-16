c61d3f46d77a0b071352820440bd42321c224c9b
Jakub <168542356+JakubSchwenkbeck@users.noreply.github.com>
2025-01-16 15:03:20 +0100

    added needed `context.report(msg)`-call

--- effekt/jvm/src/main/scala/effekt/Driver.scala
+++ effekt/jvm/src/main/scala/effekt/Driver.scala
@@ -88,7 +88,9 @@ trait Driver extends kiama.util.Compiler[EffektConfig, EffektError] { outer =>
     }
   } catch {
     case FatalPhaseError(msg) => context.report(msg)
-    case e @ CompilerPanic(msg) => generateCrashReport(e, context, config, msg)
+    case e @ CompilerPanic(msg) => 
+      context.report(msg)
+      generateCrashReport(e, context, config, msg)
 
     // when in server-mode, do not crash but report the error to avoid
     // restarting the server.
