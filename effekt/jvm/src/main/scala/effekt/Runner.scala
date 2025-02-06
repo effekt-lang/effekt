3ff77d126dfc78df3645ede42ac36a7026b4ac88
Philipp Schuster <philipp.schuster@uni-tuebingen.de>
2025-02-06 01:30:04 +0100

    Silence valgrind warnings

--- effekt/jvm/src/main/scala/effekt/Runner.scala
+++ effekt/jvm/src/main/scala/effekt/Runner.scala
@@ -94,7 +94,7 @@ trait Runner[Executable] {
    */
   def eval(executable: Executable)(using C: Context): Unit = {
     val execFile = build(executable)
-    val valgrindArgs = Seq("--leak-check=full", "--quiet", "--log-file=valgrind.log", "--error-exitcode=1")
+    val valgrindArgs = Seq("--leak-check=full", "--undef-value-errors=no", "--quiet", "--log-file=valgrind.log", "--error-exitcode=1")
     val process = if (C.config.valgrind())
       Process("valgrind", valgrindArgs ++ (execFile +: Context.config.runArgs()))
     else
