d5d949a66cbff524c31fd5985178991478b43163
Jonathan Brachthäuser <jonathan@b-studios.de>
2025-02-19 12:12:45 +0100

    Use existing rewrite infra on core

--- effekt/shared/src/main/scala/effekt/core/optimizer/DirectStyle.scala
+++ effekt/shared/src/main/scala/effekt/core/optimizer/DirectStyle.scala
@@ -2,54 +2,9 @@ package effekt
 package core
 package optimizer
 
-object DirectStyle {
+object DirectStyle extends Tree.Rewrite {
 
-  def rewrite(m: ModuleDecl): ModuleDecl = m match {
-    case ModuleDecl(path, includes, declarations, externs, definitions, exports) =>
-      ModuleDecl(path, includes, declarations, externs, definitions.map(rewrite), exports)
-  }
-
-  def rewrite(d: Toplevel): Toplevel = d match {
-    case Toplevel.Def(id, block) => Toplevel.Def(id, rewrite(block))
-    case Toplevel.Val(id, tpe, binding) => Toplevel.Val(id, tpe, rewrite(binding))
-  }
-
-  def rewrite(b: Block): Block = b match {
-    case Block.BlockVar(id, tpe, capt) => b
-    case b: Block.BlockLit => rewrite(b)
-    case Block.Unbox(pure) => Block.Unbox(rewrite(pure))
-    case Block.New(impl) => Block.New(rewrite(impl))
-  }
-
-  def rewrite(b: Block.BlockLit): Block.BlockLit = b match {
-    case Block.BlockLit(tparams, cparams, vparams, bparams, body) =>
-      Block.BlockLit(tparams, cparams, vparams, bparams, rewrite(body))
-  }
-
-  def rewrite(impl: Implementation): Implementation = impl match {
-    case Implementation(interface, operations) =>
-      Implementation(interface, operations.map { op =>
-        op.copy(body = rewrite(op.body))
-      })
-  }
-
-  def rewrite(e: Expr): Expr = e match {
-    case DirectApp(b, targs, vargs, bargs) =>
-      DirectApp(b, targs, vargs.map(rewrite), bargs.map(rewrite))
-    case p: Pure => rewrite(p)
-  }
-
-  def rewrite(p: Pure): Pure = p match {
-    case Pure.ValueVar(id, tpe) => p
-    case Pure.Literal(value, tpe) => p
-    case Pure.PureApp(b, targs, vargs) =>
-      Pure.PureApp(b, targs, vargs.map(rewrite))
-    case Pure.Make(data, tag, vargs) =>
-      Pure.Make(data, tag, vargs.map(rewrite))
-    case Pure.Box(b, capt) => Pure.Box(rewrite(b), capt)
-  }
-
-  def rewrite(s: Stmt): Stmt = s match {
+  override def stmt = {
 
     // val x = { ... return 42 }; stmt2
     //
@@ -69,48 +24,6 @@ object DirectStyle {
       else
         Val(id, tpe, rewrittenBinding, rewrittenBody)
 
-    // Congruences
-    case Def(id, block, body) =>
-      Def(id, rewrite(block), rewrite(body))
-
-    case Let(id, tpe, binding, body) =>
-      Let(id, tpe, rewrite(binding), rewrite(body))
-
-    case Return(expr) =>
-      Return(rewrite(expr))
-
-    case App(callee, targs, vargs, bargs) =>
-      App(callee, targs, vargs.map(rewrite), bargs.map(rewrite))
-
-    case Invoke(callee, method, methodTpe, targs, vargs, bargs) =>
-      Invoke(callee, method, methodTpe, targs, vargs.map(rewrite), bargs.map(rewrite))
-
-    case If(cond, thn, els) =>
-      If(rewrite(cond), rewrite(thn), rewrite(els))
-
-    case Match(scrutinee, clauses, default) =>
-      Match(rewrite(scrutinee),
-        clauses.map { case (id, bl) => (id, rewrite(bl)) },
-        default.map(rewrite))
-
-    case Reset(body) => Reset(rewrite(body))
-    case Shift(prompt, body) => Shift(prompt, rewrite(body))
-    case Resume(k, body) => Resume(k, rewrite(body))
-    case Region(body) => Region(rewrite(body))
-
-    case Alloc(id, init, region, body) =>
-      Alloc(id, rewrite(init), region, rewrite(body))
-
-    case Var(ref, init, capture, body) =>
-      Var(ref, rewrite(init), capture, rewrite(body))
-
-    case Get(id, tpe, ref, capt, body) =>
-      Get(id, tpe, ref, capt, rewrite(body))
-
-    case Put(ref, capt, value, body) =>
-      Put(ref, capt, rewrite(value), rewrite(body))
-
-    case Hole() => s
   }
 
   private def canBeDirect(s: Stmt): Boolean = s match {
