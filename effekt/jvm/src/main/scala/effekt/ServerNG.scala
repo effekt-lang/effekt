d5c13862a9dce44e4c9d9f1060cbdea943fee11c
Tim Süberkrüb <dev@timsueberkrueb.io>
2025-03-21 17:03:11 +0100

    Implement (broken) codeAction

--- effekt/jvm/src/main/scala/effekt/ServerNG.scala
+++ effekt/jvm/src/main/scala/effekt/ServerNG.scala
@@ -9,16 +9,18 @@ import scala.jdk.FunctionConverters.*
 import com.google.gson.JsonElement
 import effekt.KiamaUtils.{convertPosition, convertRange, fromLSPPosition, fromLSPRange, locationOfNode}
 import org.eclipse.lsp4j.jsonrpc.{Launcher, messages}
-import org.eclipse.lsp4j.{Diagnostic, DiagnosticRelatedInformation, DiagnosticSeverity, DidChangeConfigurationParams, DidChangeTextDocumentParams, DidChangeWatchedFilesParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams, DidSaveTextDocumentParams, DocumentSymbol, DocumentSymbolParams, Hover, HoverParams, InitializeParams, InitializeResult, InlayHint, InlayHintKind, InlayHintParams, Location, MarkupContent, PublishDiagnosticsParams, ReferenceParams, ServerCapabilities, SetTraceParams, SymbolInformation, TextDocumentIdentifier, TextDocumentSyncKind, WorkspaceFolder, Position as LSPPosition, Range as LSPRange}
+import org.eclipse.lsp4j.{CodeAction, CodeActionKind, CodeActionParams, Command, Diagnostic, DiagnosticRelatedInformation, DiagnosticSeverity, DidChangeConfigurationParams, DidChangeTextDocumentParams, DidChangeWatchedFilesParams, DidCloseTextDocumentParams, DidOpenTextDocumentParams, DidSaveTextDocumentParams, DocumentSymbol, DocumentSymbolParams, Hover, HoverParams, InitializeParams, InitializeResult, InlayHint, InlayHintKind, InlayHintParams, Location, MarkupContent, PublishDiagnosticsParams, ReferenceParams, ServerCapabilities, SetTraceParams, SymbolInformation, TextDocumentIdentifier, TextDocumentSyncKind, TextEdit, WorkspaceEdit, WorkspaceFolder, Position as LSPPosition, Range as LSPRange}
 import org.eclipse.lsp4j.services.{LanguageClient, LanguageClientAware, LanguageServer, TextDocumentService, WorkspaceService}
 import org.eclipse.lsp4j.launch.LSPLauncher
-import kiama.util.Collections.seqToJavaList
+import kiama.util.Collections.{mapToJavaMap, seqToJavaList}
 import kiama.util.Severities.{Error, Hint, Information, Severity, Warning}
 import kiama.util.{Collections, Position, Source}
 import effekt.Server.getSymbolKind
 import effekt.context.Context
+import effekt.source.Def.FunDef
+import effekt.source.Term.Hole
 import effekt.source.Tree
-import effekt.symbols.{Anon, Binder, TypePrinter, UserFunction, isSynthetic}
+import effekt.symbols.{Anon, Binder, Effects, TypePrinter, UserFunction, ValueType, isSynthetic}
 import effekt.util.PlainMessaging
 import effekt.util.messages.EffektError
 
@@ -188,14 +190,17 @@ class EffektTextDocumentService(server: ServerNG) extends TextDocumentService wi
     server.clearDiagnostics(document.getUri)
     server.getDriver.compileString(document.getUri, params.getContentChanges.get(0).getText, server.getConfig)
   }
+
   def didClose(params: DidCloseTextDocumentParams): Unit = {
     server.clearDiagnostics(params.getTextDocument.getUri)
   }
+
   def didOpen(params: DidOpenTextDocumentParams): Unit = {
     val document = params.getTextDocument
     server.clearDiagnostics(document.getUri)
     server.getDriver.compileString(document.getUri, document.getText, server.getConfig)
   }
+
   def didSave(params: DidSaveTextDocumentParams): Unit = {
     val document = params.getTextDocument
     server.clearDiagnostics(document.getUri)
@@ -314,6 +319,94 @@ class EffektTextDocumentService(server: ServerNG) extends TextDocumentService wi
     } yield hints
 
     CompletableFuture.completedFuture(Collections.seqToJavaList(hints.getOrElse(Vector())))
+  }
+
+  // LSP Code Actions
+  //
+  //
+
+  // FIXME: This is the code actions code from the previous language server implementation.
+  // It doesn't even work in the previous implementation.
+  override def codeAction(params: CodeActionParams): CompletableFuture[util.List[messages.Either[Command, CodeAction]]] = {
+    val codeActions = for {
+      position <- server.sources.get(params.getTextDocument.getUri).map { source =>
+        fromLSPPosition(params.getRange.getStart, source)
+      };
+      codeActions = for {
+        trees <- getTreesAt(position)(using server.context).toVector
+        actions <- trees.flatMap { t => action(t)(using server.context) }
+      } yield actions
+    } yield codeActions.toList
+
+    val result = codeActions.getOrElse(List[CodeAction]()).map(messages.Either.forRight[Command, CodeAction])
+    CompletableFuture.completedFuture(Collections.seqToJavaList(result))
+  }
+
+  def action(tree: Tree)(using C: Context): Option[CodeAction] = tree match {
+    case f: FunDef => inferEffectsAction(f)
+    case h: Hole => closeHoleAction(h)
+    case _ => None
+  }
+
+  def EffektCodeAction(description: String, oldNode: Any, newText: String): Option[CodeAction] = {
+    for {
+      posFrom <- server.positions.getStart(oldNode)
+      posTo <- server.positions.getFinish(oldNode)
+    } yield {
+      val textEdit = new TextEdit(convertRange(Some(posFrom), Some(posTo)), newText)
+      val changes = Map(posFrom.source.name -> seqToJavaList(List(textEdit)))
+      val workspaceEdit = new WorkspaceEdit(mapToJavaMap(changes))
+      val action = new CodeAction(description)
+      action.setKind(CodeActionKind.Refactor)
+      action.setEdit(workspaceEdit)
+      action
+    }
+  }
+
+  /**
+   * FIXME: The following comment was left on the previous Kiama-based implementation and can now be addressed:
+   *
+   * TODO it would be great, if Kiama would allow setting the position of the code action separately
+   * from the node to replace. Here, we replace the annotated return type, but would need the
+   * action on the function (since the return type might not exist in the original program).
+   *
+   * Also, it is necessary to be able to manually set the code action kind (and register them on startup).
+   * This way, we can use custom kinds like `refactor.closehole` that can be mapped to keys.
+   */
+  def inferEffectsAction(fun: FunDef)(using C: Context): Option[CodeAction] = for {
+    // the inferred type
+    (tpe, eff) <- C.inferredTypeAndEffectOption(fun)
+    // the annotated type
+    ann = for {
+      result <- fun.symbol.annotatedResult
+      effects <- fun.symbol.annotatedEffects
+    } yield (result, effects)
+    if ann.map { needsUpdate(_, (tpe, eff)) }.getOrElse(true)
+    res <- EffektCodeAction("Update return type with inferred effects", fun.ret, s": $tpe / $eff")
+  } yield res
+
+  def closeHoleAction(hole: Hole)(using C: Context): Option[CodeAction] = for {
+    holeTpe <- C.inferredTypeOption(hole)
+    contentTpe <- C.inferredTypeOption(hole.stmts)
+    if holeTpe == contentTpe
+    res <- hole match {
+      case Hole(source.Return(exp)) => for {
+        text <- server.positions.textOf(exp)
+        res <- EffektCodeAction("Close hole", hole, text)
+      } yield res
+
+      // <{ s1 ; s2; ... }>
+      case Hole(stmts) => for {
+        text <- server.positions.textOf(stmts)
+        res <- EffektCodeAction("Close hole", hole, s"locally { ${text} }")
+      } yield res
+    }
+  } yield res
+
+  def needsUpdate(annotated: (ValueType, Effects), inferred: (ValueType, Effects))(using Context): Boolean = {
+    val (tpe1, effs1) = annotated
+    val (tpe2, effs2) = inferred
+    tpe1 != tpe2 || effs1 != effs2
   }
 }
 
