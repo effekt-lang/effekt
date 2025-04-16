f669047755c976b0e4cecea71f3b048ee72ed0e7
Jiří Beneš <mail@jiribenes.com>
2025-04-16 17:42:22 +0200

    Remove debug print in Normalizer (#937)
    :)

--- effekt/shared/src/main/scala/effekt/core/optimizer/Normalizer.scala
+++ effekt/shared/src/main/scala/effekt/core/optimizer/Normalizer.scala
@@ -400,7 +400,6 @@ object Normalizer { normal =>
 
     renamedIds.foreach(copyUsage)
 
-    val newUsage = usage.collect { case (id, usage) if util.show(id) contains "foreach" => (id, usage) }
 
     // (2) substitute
     val body = substitutions.substitute(renamedLit, targs, vargs, bvars)
