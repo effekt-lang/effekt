6dcbce01e21775efcb953335a0626544f19ffdf0
Jonathan Brachthäuser <jonathan@b-studios.de>
2025-04-22 20:06:45 +0200

    Cache substitutions since they are used multiple times

--- effekt/shared/src/main/scala/effekt/typer/Constraints.scala
+++ effekt/shared/src/main/scala/effekt/typer/Constraints.scala
@@ -121,16 +121,22 @@ class Constraints(
    * Unification variables which are not in scope anymore, but also haven't been solved, yet.
    */
   private var pendingInactive: Set[CNode] = Set.empty
-
 )(using C: ErrorReporter) {
+
+  private var _substitutions: Substitutions = _
+
+  private def invalidate(): Unit = _substitutions = null
 
   /**
    * The currently known substitutions
    */
   def subst: Substitutions =
-    val types = classes.flatMap[TypeVar, ValueType] { case (k, v) => typeSubstitution.get(v).map { k -> _ } }
-    val captures = captSubstitution.asInstanceOf[Map[CaptVar, Captures]]
-    Substitutions(types, captures)
+    if _substitutions == null then {
+      val types = classes.flatMap[TypeVar, ValueType] { case (k, v) => typeSubstitution.get(v).map { k -> _ } }
+      val captures = captSubstitution.asInstanceOf[Map[CaptVar, Captures]]
+      _substitutions = Substitutions(types, captures)
+    }
+    _substitutions
 
   /**
    * Should only be called on unification variables where we do not know any types, yet
@@ -307,17 +313,22 @@ class Constraints(
     private [typer] def lowerNodes: Map[CNode, Filter] = getData(x).lowerNodes
     private [typer] def upperNodes: Map[CNode, Filter] = getData(x).upperNodes
     private def lower_=(bounds: Set[Capture]): Unit =
+      invalidate()
       captureConstraints = captureConstraints.updated(x, getData(x).copy(lower = Some(bounds)))
+
     private def upper_=(bounds: Set[Capture]): Unit =
+      invalidate()
       captureConstraints = captureConstraints.updated(x, getData(x).copy(upper = Some(bounds)))
+
     private def addLower(other: CNode, exclude: Filter): Unit =
       val oldData = getData(x)
 
       // compute the intersection of filters
       val oldFilter = oldData.lowerNodes.get(other)
       val newFilter = oldFilter.map { _ intersect exclude }.getOrElse { exclude }
-
+      invalidate()
       captureConstraints = captureConstraints.updated(x, oldData.copy(lowerNodes = oldData.lowerNodes + (other -> newFilter)))
+
     private def addUpper(other: CNode, exclude: Filter): Unit =
       val oldData = getData(x)
 
@@ -462,10 +473,11 @@ class Constraints(
    */
   private def updateSubstitution(): Unit =
     val substitution = subst
+    invalidate()
     typeSubstitution = typeSubstitution.map { case (node, tpe) => node -> substitution.substitute(tpe) }
 
   private def getNode(x: UnificationVar): Node =
-    classes.getOrElse(x, { val rep = new Node; classes += (x -> rep); rep })
+    classes.getOrElse(x, { val rep = new Node; invalidate(); classes += (x -> rep); rep })
 
   private def typeOf(n: Node): Option[ValueType] =
     typeSubstitution.get(n)
