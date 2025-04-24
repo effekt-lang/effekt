6488072038eea35547b3f23f74320ed16107cea4
Marvin Borner <git@marvinborner.de>
2025-04-24 17:41:02 +0200

    Replace kiama relation tree with explicit DFS
    I experimented with using Tree.Query, but we would still need to recurse
    into individual fields (like ops in InterfaceDefs), so we can also just
    recurse explicitly entirely. In the actual renderer of the
    documentation, we will now need to recurse using "rest" fields or
    specific arrays like "ops", but I think that's okay.

--- effekt/shared/src/main/scala/effekt/util/DocumentationGenerator.scala
+++ effekt/shared/src/main/scala/effekt/util/DocumentationGenerator.scala
@@ -4,7 +4,7 @@ import effekt.source.*
 import kiama.util.Source
 import scala.collection.immutable.HashMap
 
-// TODO: positions
+// TODO: include spans
 trait DocumentationGenerator {
   type Documentation = HashMap[String, DocValue]
   type EffektTree[T <: Tree] = kiama.relation.Tree[AnyRef & Product, T]
@@ -12,19 +12,19 @@ trait DocumentationGenerator {
   // A recursive structure that resembles JSON
   sealed trait DocValue
   case class DocString(value: String) extends DocValue
-  case class DocArray(value: Vector[DocValue]) extends DocValue
+  case class DocArray(value: List[DocValue]) extends DocValue
   case class DocObject(value: Documentation) extends DocValue
 
   def empty = DocObject(HashMap.empty)
   def str(s: String) = DocString(s)
-  def arr(docs: Vector[DocValue]) = DocArray(docs)
+  def arr(docs: List[DocValue]) = DocArray(docs)
   def obj(doc: Documentation) = DocObject(doc)
 
   def generate(doc: Doc): DocString = str(s"${doc.getOrElse("").replace("\"", "\\\"")}")
 
   def generateTparams(list: List[Id]): DocString = str(s"[${list.map(_.name).mkString(", ")}]")
 
-  def generate(effects: Effects): DocValue = arr(effects.effs.map(generate).toVector)
+  def generate(effects: Effects): DocValue = arr(effects.effs.map(generate))
 
   def generate(effectful: Effectful): DocValue = effectful match {
     case Effectful(tpe, eff) => obj(HashMap(
@@ -47,7 +47,7 @@ trait DocumentationGenerator {
     case BlockTypeRef(id, args) => obj(HashMap(
       "kind" -> str("BlockTypeRef"),
       "id" -> generate(id),
-      "args" -> arr(args.map(generate).toVector)
+      "args" -> arr(args.map(generate))
     ))
   }
 
@@ -55,7 +55,7 @@ trait DocumentationGenerator {
   def generate(id: IdDef): DocValue = str(id.name)
   def generate(id: IdRef): DocValue = str(id.name)
 
-  def generate(capt: CaptureSet): DocValue = arr(capt.captures.map(generate).toVector)
+  def generate(capt: CaptureSet): DocValue = arr(capt.captures.map(generate))
 
   def generate(tpe: ValueType): DocValue = tpe match {
     case ValueTypeTree(tpe) => ??? // ignore?
@@ -67,7 +67,7 @@ trait DocumentationGenerator {
     case ValueTypeRef(id, args) => obj(HashMap(
       "kind" -> str("BoxedType"),
       "id" -> generate(id),
-      "args" -> arr(args.map(generate).toVector)
+      "args" -> arr(args.map(generate))
     ))
   }
 
@@ -78,7 +78,7 @@ trait DocumentationGenerator {
         "id" -> generate(id),
         "tpe" -> tpe.map(generate).getOrElse(empty),
       ))
-  }.toVector)
+  })
 
   def generateBparams(list: List[BlockParam]): DocValue = arr(list.map {
     case BlockParam(id, tpe) =>
@@ -87,143 +87,167 @@ trait DocumentationGenerator {
         "id" -> generate(id),
         "tpe" -> tpe.map(generate).getOrElse(empty),
       ))
-  }.toVector)
+  })
 
-  def generate(definition: Def): Documentation = definition match {
-    case Def.FunDef(id, tparams, vparams, bparams, ret, body, doc) => HashMap(
+  def generate(constructor: Constructor): DocValue = constructor match {
+    case Constructor(id, tparams, vparams, doc) => obj(HashMap(
+      "kind" -> str("Constructor"),
+      "id" -> generate(id),
+      "tparams" -> generateTparams(tparams),
+      "vparams" -> generateVparams(vparams),
+      "doc" -> generate(doc),
+    ))
+  }
+
+  def generate(operation: Operation): DocValue = operation match {
+    case Operation(id, tparams, vparams, bparams, ret, doc) => obj(HashMap(
+      "kind" -> str("Operation"),
+      "tparams" -> generateTparams(tparams),
+      "vparams" -> generateVparams(vparams),
+      "bparams" -> generateBparams(bparams),
+      "ret" -> generate(ret),
+      "doc" -> generate(doc),
+    ))
+  }
+
+  def generate(module: Include): DocValue = module match {
+    case Include(path) => obj(HashMap(
+      "kind" -> str("Include"),
+      "path" -> str(path),
+    ))
+  }
+
+  def generate(definition: Def): DocValue = definition match {
+    case Def.FunDef(id, tparams, vparams, bparams, ret, body, doc) => obj(HashMap(
       "kind" -> str("FunDef"),
       "id" -> generate(id),
       "tparams" -> generateTparams(tparams),
       "vparams" -> generateVparams(vparams),
       "bparams" -> generateBparams(bparams),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.ValDef(id, annot, binding, doc) => HashMap(
+    case Def.ValDef(id, annot, binding, doc) => obj(HashMap(
       "kind" -> str("ValDef"),
       "id" -> generate(id),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.RegDef(id, annot, region, binding, doc) => HashMap(
+    case Def.RegDef(id, annot, region, binding, doc) => obj(HashMap(
       "kind" -> str("RegDef"),
       "id" -> generate(id),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.VarDef(id, annot, binding, doc) => HashMap(
+    case Def.VarDef(id, annot, binding, doc) => obj(HashMap(
       "kind" -> str("VarDef"),
       "id" -> generate(id),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.DefDef(id, annot, block, doc) => HashMap(
+    case Def.DefDef(id, annot, block, doc) => obj(HashMap(
       "kind" -> str("DefDef"),
       "id" -> generate(id),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.NamespaceDef(id, definitions, doc) => HashMap(
+    case Def.NamespaceDef(id, definitions, doc) => obj(HashMap(
       "kind" -> str("NamespaceDef"),
       "id" -> generate(id),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.InterfaceDef(id, tparams, ops, doc) => HashMap(
+    case Def.InterfaceDef(id, tparams, ops, doc) => obj(HashMap(
       "kind" -> str("NamespaceDef"),
       "id" -> generate(id),
       "tparams" -> generateTparams(tparams),
-      "ops" -> arr(generate(ops.toVector).map(obj)),
+      "ops" -> arr(ops.map(generate)),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.DataDef(id, tparams, ctors, doc) => HashMap(
+    case Def.DataDef(id, tparams, ctors, doc) => obj(HashMap(
       "kind" -> str("DataDef"),
       "id" -> generate(id),
       "tparams" -> generateTparams(tparams),
+      "ctors" -> arr(ctors.map(generate)),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.RecordDef(id, tparams, fields, doc) => HashMap(
+    case Def.RecordDef(id, tparams, fields, doc) => obj(HashMap(
       "kind" -> str("RecordDef"),
       "id" -> generate(id),
       "tparams" -> generateTparams(tparams),
+      "fields" -> generateVparams(fields),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.TypeDef(id, tparams, tpe, doc) => HashMap(
+    case Def.TypeDef(id, tparams, tpe, doc) => obj(HashMap(
       "kind" -> str("TypeDef"),
       "id" -> generate(id),
       "tparams" -> generateTparams(tparams),
+      "tpe" -> generate(tpe),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.EffectDef(id, tparams, effs, doc) => HashMap(
+    case Def.EffectDef(id, tparams, effs, doc) => obj(HashMap(
       "kind" -> str("EffectDef"),
       "id" -> generate(id),
       "tparams" -> generateTparams(tparams),
+      "effs" -> generate(effs),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.ExternType(id, tparams, doc) => HashMap(
+    case Def.ExternType(id, tparams, doc) => obj(HashMap(
       "kind" -> str("ExternType"),
       "id" -> generate(id),
       "tparams" -> generateTparams(tparams),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.ExternDef(capture, id, tparams, vparams, bparams, ret, bodies, doc) => HashMap(
+    case Def.ExternDef(capture, id, tparams, vparams, bparams, ret, bodies, doc) => obj(HashMap(
       "kind" -> str("ExternDef"),
       "id" -> generate(id),
       "tparams" -> generateTparams(tparams),
       "vparams" -> generateVparams(vparams),
       "bparams" -> generateBparams(bparams),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.ExternResource(id, tpe, doc) => HashMap(
+    case Def.ExternResource(id, tpe, doc) => obj(HashMap(
       "kind" -> str("ExternResource"),
       "id" -> generate(id),
+      "tpe" -> generate(tpe),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.ExternInterface(id, tparams, doc) => HashMap(
+    case Def.ExternInterface(id, tparams, doc) => obj(HashMap(
       "kind" -> str("ExternInterface"),
       "id" -> generate(id),
       "tparams" -> generateTparams(tparams),
       "doc" -> generate(doc),
-    )
+    ))
 
-    case Def.ExternInclude(featureFlag, path, contents, id, doc) => HashMap(
+    case Def.ExternInclude(featureFlag, path, contents, id, doc) => obj(HashMap(
       "kind" -> str("ExternInclude"),
+      "path" -> str(path),
       "id" -> generate(id),
       "doc" -> generate(doc),
-    )
+    ))
   }
 
-  def generate(operation: Operation): Documentation = operation match {
-    case Operation(id, tparams, vparams, bparams, ret, doc) => HashMap(
-      "kind" -> str("operation"),
-      "tparams" -> generateTparams(tparams),
-      "vparams" -> generateVparams(vparams),
-      "bparams" -> generateBparams(bparams),
-      "ret" -> generate(ret),
-      "doc" -> generate(doc)
-    )
-  }
-
-  def generate(nodes: Vector[Tree]): Vector[Documentation] = nodes.map(generate)
-
-  def generate(node: Tree): Documentation = node match {
-    case n: Def => generate(n)
-    case n: Operation => generate(n)
-    // case n: ModuleDecl => generate(n)
-    case _ => HashMap.empty
+  def generate(module: ModuleDecl): DocValue = module match {
+    case ModuleDecl(path, includes, defs, doc) => obj(HashMap(
+      "kind" -> str("ModuleDecl"),
+      "path" -> str(path),
+      "includes" -> arr(includes.map(generate)),
+      "defs" -> arr(defs.map(generate)),
+      "doc" -> generate(doc),
+    ))
   }
 }
 
-case class JSONDocumentationGenerator(ast: ModuleDecl, name: String = "") extends Source, DocumentationGenerator {
-  def toJSON(values: Vector[DocValue]): String = s"[${values.map(toJSON).mkString(",")}]"
+case class JSONDocumentationGenerator(ast: ModuleDecl, name: String = "")(using C: Context) extends Source, DocumentationGenerator {
+  def toJSON(values: List[DocValue]): String = s"[${values.map(toJSON).mkString(",")}]"
 
   def toJSON(docValue: DocValue): String = docValue match {
     case DocString(str) => s"\"${str}\""
@@ -239,20 +263,8 @@ case class JSONDocumentationGenerator(ast: ModuleDecl, name: String = "") extend
     s"{${jsonPairs.mkString(",")}}"
   }
 
-  def dfsJSON(node: Tree)(visit: Tree => String): StringBuilder = {
-    val res = new StringBuilder(s"{\"data\": ${visit(node)}, \"children\": [")
-    var children = kiama.relation.TreeRelation.treeChildren(node)
-      .collect { case t: Def => t }
-      .map { child => dfsJSON(child)(visit) }
-    res.append(children.map(_.toString).mkString(","))
-    res.append("])")
-    res
-  }
-
   lazy val content = {
-    val tree = new EffektTree(ast)
-    val docs = dfsJSON(tree.root) { t => toJSON(generate(t)) }
-
+    val docs = toJSON(generate(ast))
     s"{\"source\": \"${name}\", \"documentation\": [${docs}]}"
   }
 }
