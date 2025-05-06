c0f4f51e29047162d3c71082b134d6da91396c6c
Tim Süberkrüb <dev@timsueberkrueb.io>
2025-05-06 13:37:42 +0200

    Use fullSpan for body in REPL

--- effekt/jvm/src/main/scala/effekt/Repl.scala
+++ effekt/jvm/src/main/scala/effekt/Repl.scala
@@ -316,7 +316,7 @@ class Repl(driver: Driver) extends REPL[Tree, EffektConfig, EffektError] {
       val fullSpan = Span(source, 0, source.content.length, origin = Origin.Synthesized)
       ModuleDecl("interactive", includes,
         definitions :+ FunDef(IdDef("main", fakeSpan), Many.empty(fakeSpan), Many.empty(fakeSpan), Many.empty(fakeSpan), Maybe.None(fakeSpan),
-          body, fakeSpan), fullSpan)
+          body, fullSpan), fullSpan)
     }
 
     def makeEval(source: Source, expr: Term): ModuleDecl = {
