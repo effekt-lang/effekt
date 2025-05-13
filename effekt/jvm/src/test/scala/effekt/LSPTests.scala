6be7ca99908258e1370836a4cee938addc9c2fac
Jiří Beneš <mail@jiribenes.com>
2025-05-13 17:57:47 +0200

    ... and also update the LSPTests

--- effekt/jvm/src/test/scala/effekt/LSPTests.scala
+++ effekt/jvm/src/test/scala/effekt/LSPTests.scala
@@ -1139,7 +1139,7 @@ class LSPTests extends FunSuite {
       val expectedIRContents =
         raw"""ModuleDecl(
              |  test,
-             |  List(effekt, option, list, result, exception, array, string, ref),
+             |  List(effekt, option, list, result, exception, array, char, string, ref),
              |  Nil,
              |  Nil,
              |  List(
