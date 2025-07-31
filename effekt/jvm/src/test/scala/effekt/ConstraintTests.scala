acc120d78b80e3b7a475defd19fb1b4413b40bec
Tim Süberkrüb <dev@timsueberkrueb.io>
2025-07-31 11:51:04 +0200

    Rip out kiama positions

--- effekt/jvm/src/test/scala/effekt/ConstraintTests.scala
+++ effekt/jvm/src/test/scala/effekt/ConstraintTests.scala
@@ -2,11 +2,9 @@ package effekt
 package typer
 package constraints
 
-import effekt.context.Context
 import effekt.source.NoSource
 import effekt.symbols.*
 import effekt.util.messages.{DebugMessaging, ErrorReporter, FatalPhaseError}
-import kiama.util.Positions
 import effekt.core.TestContext
 
 import scala.language.implicitConversions
@@ -15,7 +13,7 @@ abstract class ConstraintTests extends munit.FunSuite {
 
   object messages extends DebugMessaging
 
-  given ErrorReporter with { var focus = NoSource; val messaging = messages; val positions = new Positions }
+  given ErrorReporter with { var focus = NoSource; val messaging = messages }
 
   lazy val scope = { val s = new TestContext; s.enterScope(); s }
 
