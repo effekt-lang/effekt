effekt-lang/effekt

commit a0033bfe1ca076bf2d3915e56a32ba82704a5df3

Drop matches on TBottom in optimizer

effekt/shared/src/main/scala/effekt/core/optimizer/Normalizer.scala
@@ -213,6 +213,9 @@ object Normalizer { normal =>
           Stmt.Invoke(normalized.shared, method, methodTpe, targs, vargs.map(normalize), bargs.map(normalize))
       }
 
+    case Stmt.Match(scrutinee, clauses, default) if scrutinee.tpe == Type.TBottom =>
+      Stmt.Return(normalize(scrutinee))
+
     case Stmt.Match(scrutinee, clauses, default) => active(scrutinee) match {
       case Pure.Make(data, tag, targs, vargs) if clauses.exists { case (id, _) => id == tag } =>
         val clause: BlockLit = clauses.collectFirst { case (id, cl) if id == tag => cl }.get

commit 97df655eff616f227a06ae849ba604b7eac244c5

Try to fix performance regression introduced by a0033bf

effekt/shared/src/main/scala/effekt/core/optimizer/Normalizer.scala
@@ -161,6 +161,14 @@ object Normalizer { normal =>
       case other => other // stuck
     }
 
+  def isAbortive(s: Stmt)(using C: Context): Boolean =
+    s.tpe == Type.TBottom || (s match {
+      case Stmt.Match(scrutinee, clauses, default) => clauses.isEmpty && default.isEmpty
+      case Stmt.Shift(p, BlockLit(tparams, cparams, vparams, BlockParam(k, _,_) :: Nil, body2)) =>
+        !Variables.free(body2).containsBlock(k)
+      case _ => false
+    })
+
   def normalize(s: Stmt)(using C: Context): Stmt = s match {
 
     // see #798 for context (led to stack overflow)
@@ -256,6 +264,13 @@ object Normalizer { normal =>
         case Stmt.Match(sc, List((id2, BlockLit(tparams2, cparams2, vparams2, bparams2, body2))), None) =>
           Stmt.Match(sc, List((id2, BlockLit(tparams2, cparams2, vparams2, bparams2,
             normalizeVal(id, tpe, body2, body)))), None)
+
+        // if continuation is used linearly, push down
+        case norm @ Stmt.If(cond, thn, els) if isAbortive(thn) =>
+          Stmt.If(cond, thn, normalizeVal(id, tpe, els, body))
+
+        case norm @ Stmt.If(cond, thn, els) if isAbortive(els) =>
+          Stmt.If(cond, normalizeVal(id, tpe, thn, body), els)
 
         // These rewrites do not seem to contribute a lot given their complexity...
         // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv