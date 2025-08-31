ec12f13fb0c99c52b5b9c4ebef1a5bdb4c86bae2
Mattis Böckle <mattisderbock@gmail.com>
2025-08-31 19:28:25 +0200

    Generate show instances for types with no targs

--- effekt/shared/src/main/scala/effekt/generator/llvm/LLVM.scala
+++ effekt/shared/src/main/scala/effekt/generator/llvm/LLVM.scala
@@ -52,7 +52,7 @@ class LLVM extends Compiler[String] {
   // -----------------------------------
   object steps {
     // intermediate steps for VSCode
-    val afterCore = allToCore(Core) andThen Aggregate andThen optimizer.Optimizer andThen core.PolymorphismBoxing
+    val afterCore = allToCore(Core) andThen Aggregate andThen optimizer.Optimizer andThen core.PolymorphismBoxing andThen core.Show
     val afterMachine = afterCore andThen Machine map { case (mod, main, prog) => prog }
     val afterLLVM = afterMachine map {
       case machine.Program(decls, defns, entry) =>
