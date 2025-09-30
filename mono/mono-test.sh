5119aa52cb75748319b4f56f613ce76d1f062197
Mattis Böckle <mattisderbock@gmail.com>
2025-09-30 23:37:22 +0200

    Add a temporary folder for mono examples

---
+++ mono/mono-test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+sbt "project effektJVM" \
+	"run mono/mono1.effekt" \
+	"run mono/mono2.effekt" \
+	"run mono/mono3.effekt" \
+	"run mono/mono4.effekt" \
+	"run mono/mono5.effekt" \
+	"run mono/mono6.effekt" \
+	"run mono/mono7.effekt" \
+	"run mono/mono8.effekt" \
+	"run mono/mono9.effekt" \
+	"run mono/mono10.effekt" \
+	"run mono/mono11.effekt" \
+	"run mono/mono12.effekt" \
+	"run mono/mono13.effekt" \
