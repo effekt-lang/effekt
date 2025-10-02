a0da98a40ed4031a3e95d6c397259b8562b5711b
Jiří Beneš <mail@jiribenes.com>
2025-10-02 16:34:59 +0200

    Revert inlining heuristics

--- effekt/jvm/src/main/scala/effekt/EffektConfig.scala
+++ effekt/jvm/src/main/scala/effekt/EffektConfig.scala
@@ -124,7 +124,7 @@ class EffektConfig(args: Seq[String]) extends REPLConfig(args.takeWhile(_ != "--
   val maxInlineSize: ScallopOption[Long] = opt(
     "max-inline-size",
     descr = "Maximum size (number of core tree-nodes) of a function considered by the inliner",
-    default = Some(20L),
+    default = Some(50L),
     noshort = true,
     group = advanced
   )
