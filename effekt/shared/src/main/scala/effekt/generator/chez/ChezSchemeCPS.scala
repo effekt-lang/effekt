4472441623bcd7542f5fb5de4ac8ed014e201cb7
Martin Ilgner <martinilgner22@gmail.com>
2025-10-09 11:22:02 +0200

    Be able to compile simple programs :)

--- effekt/shared/src/main/scala/effekt/generator/chez/ChezSchemeCPS.scala
+++ effekt/shared/src/main/scala/effekt/generator/chez/ChezSchemeCPS.scala
@@ -9,43 +9,62 @@ import kiama.output.PrettyPrinterTypes.Document
 
 class ChezSchemeCPS extends Compiler[String] {
 
-    override def extension: String = ".ss"
+  override def extension: String = ".ss"
 
-    override def supportedFeatureFlags: List[String] = List("chez", "chez-cps")
+  override def supportedFeatureFlags: List[String] = List("chez", "chezCPS")
 
-    override def prettyIR(source: Source, stage: Stage)(using Context): Option[Document] = ???
+  override def prettyIR(source: Source, stage: Stage)(using C: Context): Option[Document] = stage match {
+    case Stage.Core if C.config.optimize() => Optimized(source).map { (_, _, res) => core.PrettyPrinter.format(res) }
+    case Stage.CPS => CPSTransformed(source).map { (_, _, _, res) => cps.PrettyPrinter.format(res) }
+    case Stage.Core => Core(source).map { res => core.PrettyPrinter.format(res.core) }
+    case Stage.Machine => None
+    case Stage.Target => LSP(source)
+  }
 
-    override def treeIR(source: Source, stage: Stage)(using Context): Option[Any] = ???
+  override def treeIR(source: Source, stage: Stage)(using Context): Option[Any] = stage match {
+    case Stage.Core => Core(source).map { res => res.core }
+    case Stage.CPS => CPSTransformed(source).map { (_, _, _, res) => res }
+    case Stage.Machine => None
+    case Stage.Target => Chez(source)
+  }
 
-    override def compile(source: Source)(using C: Context) = Compile(source)
+  override def compile(source: Source)(using C: Context) = Chez(source)
 
-    // The Compilation Pipeline
-    // ------------------------
-    // Source => Core => CPS => ChezCPS
-    lazy val Core = Phase.cached("core") {
-        Frontend andThen Middleend
-    }
+  // The Compilation Pipeline
+  // ------------------------
+  // Source => Core => CPS => Chez
+  lazy val Core = Phase.cached("core") {
+    Frontend andThen Middleend
+  }
 
-    lazy val Optimized = allToCore(Core) andThen Aggregate andThen Optimizer andThen DropBindings map {
-        case input @ CoreTransformed(source, tree, mod, core) =>
-        val mainSymbol = Context.ensureMainExists(mod)
-        val mainFile = path(mod)
-        (mainSymbol, mainFile, core)
-    }
+  lazy val Optimized = allToCore(Core) andThen Aggregate andThen Optimizer map {
+    case input @ CoreTransformed(source, tree, mod, core) =>
+      val mainSymbol = Context.ensureMainExists(mod)
+      val mainFile = path(mod)
+      (mainSymbol, mainFile, core)
+  }
 
-    lazy val CPSTransformed = Optimized map {
-        case (mainSymbol, mainFile, core) =>
-        val cpsTransformed = effekt.cps.Transformer.transform(core)
-        val contified = cps.Contify.rewrite(cpsTransformed)
-        (mainSymbol, mainFile, core, contified)
-    }
+  lazy val CPSTransformed = Optimized map {
+    case (mainSymbol, mainFile, core) =>
+      val cpsTransformed = effekt.cps.Transformer.transform(core)
+      val contified = cps.Contify.rewrite(cpsTransformed)
+      (mainSymbol, mainFile, core, contified)
+  }
 
-    lazy val Compile = CPSTransformed map {
-        case (mainSymbol, mainFile, core, cps) =>
-        val doc = pretty(TransformerCPS.compile(cps, core, mainSymbol))
-        (Map(mainFile -> doc.layout), mainFile)
-    }
+  lazy val Chez = CPSTransformed map {
+    case (mainSymbol, mainFile, core, cps) =>
+      val compiled = TransformerCPS.compile(cps, mainSymbol)
+      val doc = pretty(chez.Let(Nil, compiled))
+      (Map(mainFile -> doc.layout), mainFile)
+  }
 
-    private def pretty(block: chez.Block): Document =
-        chez.PrettyPrinterCPS.format(block)
+  // TODO: Only show generated code
+  lazy val LSP = CPSTransformed map {
+    case (mainSymbol, mainFile, core, cps) =>
+      val compiled = TransformerCPS.compile(cps, mainSymbol)
+      pretty(chez.Let(Nil, compiled))
+  }
+
+  def pretty(expr: chez.Expr): Document =
+    chez.PrettyPrinter.pretty(chez.PrettyPrinter.toDoc(expr), 100)
 }
--- effekt/shared/src/main/scala/effekt/generator/chez/TransformerCPS.scala
+++ effekt/shared/src/main/scala/effekt/generator/chez/TransformerCPS.scala
@@ -2,8 +2,73 @@ package effekt
 package generator
 package chez
 
-import effekt.context.Context
+import cps.*
+import effekt.symbols.Symbol
+import effekt.core
 
 object TransformerCPS {
-    def compile(input: cps.ModuleDecl, coreModule: core.ModuleDecl, mainSymbol: symbols.TermSymbol)(using Context): chez.Block = ???
+
+  // Defined in chez/cps/effekt.ss
+  val RUN_TOP_LEVEL = "run-top-level"
+
+  def compile(input: cps.ModuleDecl, mainSymbol: symbols.TermSymbol): chez.Block = {
+    val definitions = input.definitions.map(toChez)
+    val externs = input.externs.map(toChez)
+    val runMain = Builtin(RUN_TOP_LEVEL, nameRef(mainSymbol))
+    chez.Block(externs ++ definitions, Nil, runMain)
+  }
+
+  def toChez(definition: cps.ToplevelDefinition): chez.Def = definition match {
+    case ToplevelDefinition.Def(id, block) => chez.Constant(nameDef(id), toChez(block))
+    case ToplevelDefinition.Let(id, expr) => chez.Constant(nameDef(id), toChez(expr))
+    case _ => ???
+  }
+
+  def toChez(extern: cps.Extern): chez.Def = extern match {
+    case Extern.Def(id, vparams, bparams, async, body) if !async =>
+      val params = (vparams ++ bparams).map(nameDef)
+      chez.Function(nameDef(id), params, toChez(body))
+    case Extern.Include(_, contents) =>
+      chez.RawDef(contents)
+    case _ => ???
+  }
+
+  def toChez(externBody: cps.ExternBody): chez.Expr = externBody match {
+    case ExternBody.StringExternBody(_, contents) =>
+      RawExpr(contents.strings, contents.args.map(toChez))
+    case ExternBody.Unsupported(err) => ???
+  }
+
+  def toChez(block: cps.Block): chez.Expr = block match {
+    case BlockVar(id) => chez.Variable(nameRef(id))
+    case BlockLit(vparams, bparams, ks, k, body) =>
+      val params = vparams ++ bparams ++ List(ks, k)
+      chez.Lambda(params.map(nameDef), toChez(body))
+    case _ => ???
+  }
+
+  def toChez(stmt: cps.Stmt): chez.Expr = stmt match {
+    case App(callee, vargs, bargs, ks, k) =>
+      val args = vargs.map(toChez) ++ bargs.map(toChez)
+              ++ List(toChez(ks.id), toChez(k))
+      chez.Call(toChez(callee), args)
+    case Jump(k, vargs, ks) =>
+      val args = vargs.map(toChez) :+ toChez(ks.id)
+      chez.Call(toChez(k), args)
+    case _ => ???
+  }
+
+  def toChez(expr: cps.Expr): chez.Expr = expr match {
+    case ValueVar(id) => toChez(id)
+    case Literal(()) => chez.RawValue("(void)")
+    case Literal(value) => chez.RawValue(value.toString())
+    case _ => ???
+  }
+
+  def toChez(cont: cps.Cont): chez.Expr = cont match {
+    case Cont.ContVar(id) => toChez(id)
+    case _ => ???
+  }
+
+  def toChez(id: Symbol): chez.Variable = Variable(nameRef(id))
 }
