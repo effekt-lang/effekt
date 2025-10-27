101816e78c054725ce68dfbe85872945b4f9ef79
Tim Süberkrüb <dev@timsueberkrueb.io>
2025-10-27 16:36:47 +0100

    Fix tail resumptive analysis for vars (#1154)
    Fixes #1153.

--- effekt/shared/src/main/scala/effekt/core/optimizer/RemoveTailResumptions.scala
+++ effekt/shared/src/main/scala/effekt/core/optimizer/RemoveTailResumptions.scala
@@ -33,9 +33,12 @@ object RemoveTailResumptions {
       case Stmt.Match(scrutinee, clauses, default) => !freeInExpr(scrutinee) && clauses.forall {
         case (_, BlockLit(tparams, cparams, vparams, bparams, body)) => tailResumptive(k, body)
       } && default.forall { stmt => tailResumptive(k, stmt) }
-      case Stmt.Region(BlockLit(tparams, cparams, vparams, bparams, body)) => tailResumptive(k, body)
+      case Stmt.Region(BlockLit(tparams, cparams, vparams, bparams, body)) => false
       case Stmt.Alloc(id, init, region, body) => tailResumptive(k, body) && !freeInExpr(init)
-      case Stmt.Var(ref, init, capture, body) => tailResumptive(k, body) && !freeInExpr(init)
+      // Conceptually, a mutable variable definition can be seen as a handler for get and put operations.
+      // Treating this as tail-resumptive leads to a failure of semantics preservation.
+      // See https://github.com/effekt-lang/effekt/issues/1153 for an example.
+      case Stmt.Var(ref, init, capture, body) => false
       case Stmt.Get(ref, annotatedCapt, tpe, id, body) => tailResumptive(k, body)
       case Stmt.Put(ref, annotatedCapt, value, body) => tailResumptive(k, body) && !freeInExpr(value)
       case Stmt.Reset(BlockLit(tparams, cparams, vparams, bparams, body)) => false
