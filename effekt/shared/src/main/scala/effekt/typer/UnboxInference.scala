1f66bc529fb1d8c347cf90039503905b864f3ac5
Jiří Beneš <mail@jiribenes.com>
2025-10-29 15:10:48 +0100

    Fix #1165: improve position info for UnboxInference errors (#1167)
    Fix #1165 by attaching position info to messages originating in
    UnboxInference: when we visit a tree node, we should "zoom in" on it in
    the error reporting.

--- effekt/shared/src/main/scala/effekt/typer/UnboxInference.scala
+++ effekt/shared/src/main/scala/effekt/typer/UnboxInference.scala
@@ -53,7 +53,7 @@ object UnboxInference extends Phase[NameResolved, NameResolved] {
       // TODO maybe we should synthesize a call to get here already?
       case sym: (ValueSymbol | symbols.RefBinder) => v
       case sym: BlockSymbol =>
-        C.error(pp"Computation ${sym} is used in an expression position, which requires boxing (e.g. `box ${sym}`)")
+        C.error(pp"Computation ${sym.name.name} is used in an expression position, which requires boxing (e.g. `box ${sym.name.name}`)")
         v
     }
 
@@ -215,7 +215,7 @@ object UnboxInference extends Phase[NameResolved, NameResolved] {
   /**
    * Copies all annotations and position information from source to target
    */
-  def visit[T <: Tree, R <: Tree](source: T)(block: T => R)(using C: Context): R = {
+  def visit[T <: Tree, R <: Tree](source: T)(block: T => R)(using C: Context): R = C.at(source) {
     val target = block(source)
     target.inheritPosition(source)
     C.copyAnnotations(source, target)
