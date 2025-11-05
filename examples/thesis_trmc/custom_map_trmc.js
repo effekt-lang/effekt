c5f86e38261de7b5bb7f74eb1c6a33317e03890c
Arkinul <heinrich.timphus@web.de>
2025-11-05 22:29:28 +0100

    transformed map() in JS, first try

--- examples/thesis_trmc/custom_map_trmc.js
+++ examples/thesis_trmc/custom_map_trmc.js
@@ -1,47 +1,47 @@
 const $effekt = {  };
 
 class Nil_0 {
-    constructor() {
-        this.__tag = 0;
+  constructor() {
+    this.__tag = 0;
+  }
+  __reflect() {
+    return { __tag: 0, __name: "Nil", __data: [] };
+  }
+  __equals(other5896) {
+    if (!other5896) {
+      return false;
     }
-    __reflect() {
-        return { __tag: 0, __name: "Nil", __data: [] };
+    if (!($effekt.equals(this.__tag, other5896.__tag))) {
+      return false;
     }
-    __equals(other5896) {
-        if (!other5896) {
-            return false;
-        }
-        if (!($effekt.equals(this.__tag, other5896.__tag))) {
-            return false;
-        }
-        return true;
-    }
+    return true;
+  }
 }
 
 class Cons_0 {
-    constructor(head_0, tail_0) {
-        this.__tag = 1;
-        this.head_0 = head_0;
-        this.tail_0 = tail_0;
+  constructor(head_0, tail_0) {
+    this.__tag = 1;
+    this.head_0 = head_0;
+    this.tail_0 = tail_0;
+  }
+  __reflect() {
+    return { __tag: 1, __name: "Cons", __data: [this.head_0, this.tail_0] };
+  }
+  __equals(other5897) {
+    if (!other5897) {
+      return false;
     }
-    __reflect() {
-        return { __tag: 1, __name: "Cons", __data: [this.head_0, this.tail_0] };
+    if (!($effekt.equals(this.__tag, other5897.__tag))) {
+      return false;
     }
-    __equals(other5897) {
-        if (!other5897) {
-            return false;
-        }
-        if (!($effekt.equals(this.__tag, other5897.__tag))) {
-            return false;
-        }
-        if (!($effekt.equals(this.head_0, other5897.head_0))) {
-            return false;
-        }
-        if (!($effekt.equals(this.tail_0, other5897.tail_0))) {
-            return false;
-        }
-        return true;
+    if (!($effekt.equals(this.head_0, other5897.head_0))) {
+      return false;
     }
+    if (!($effekt.equals(this.tail_0, other5897.tail_0))) {
+      return false;
+    }
+    return true;
+  }
 }
 
 // Complexity of state:
@@ -53,65 +53,65 @@ class Cons_0 {
 const Mem = null
 
 function Arena() {
-    const s = {
-        root: { value: Mem },
-        generation: 0,
-        fresh: (v) => {
-            const r = {
-                value: v,
-                generation: s.generation,
-                store: s,
-                set: (v) => {
-                    const s = r.store
-                    const r_gen = r.generation
-                    const s_gen = s.generation
+  const s = {
+    root: { value: Mem },
+    generation: 0,
+    fresh: (v) => {
+      const r = {
+        value: v,
+        generation: s.generation,
+        store: s,
+        set: (v) => {
+          const s = r.store
+          const r_gen = r.generation
+          const s_gen = s.generation
 
-                    if (r_gen == s_gen) {
-                        r.value = v;
-                    } else {
-                        const root = { value: Mem }
-                        // update store
-                        s.root.value = { ref: r, value: r.value, generation: r_gen, root: root }
-                        s.root = root
-                        r.value = v
-                        r.generation = s_gen
-                    }
-                }
-            };
-            return r
-        },
-        // not implemented
-        newRegion: () => s
-    };
-    return s
+          if (r_gen == s_gen) {
+            r.value = v;
+          } else {
+            const root = { value: Mem }
+            // update store
+            s.root.value = { ref: r, value: r.value, generation: r_gen, root: root }
+            s.root = root
+            r.value = v
+            r.generation = s_gen
+          }
+        }
+      };
+      return r
+    },
+    // not implemented
+    newRegion: () => s
+  };
+  return s
 }
 
 function snapshot(s) {
-    const snap = { store: s, root: s.root, generation: s.generation }
-    s.generation = s.generation + 1
-    return snap
+  const snap = { store: s, root: s.root, generation: s.generation }
+  s.generation = s.generation + 1
+  return snap
 }
 
 function reroot(n) {
-    if (n.value === Mem) return;
+  if (n.value === Mem) return;
 
-    const diff = n.value
-    const r = diff.ref
-    const v = diff.value
-    const g = diff.generation
-    const n2 = diff.root
-    reroot(n2)
-    n.value = Mem
-    n2.value = { ref: r, value: r.value, generation: r.generation, root: n}
-    r.value = v
-    r.generation = g
+  const diff = n.value
+  const r = diff.ref
+  const v = diff.value
+  const g = diff.generation
+  const n2 = diff.root
+  reroot(n2)
+  n.value = Mem
+  n2.value = { ref: r, value: r.value, generation: r.generation, root: n}
+  r.value = v
+  r.generation = g
 }
 
 function restore(store, snap) {
-    // linear in the number of modifications...
-    reroot(snap.root)
-    store.root = snap.root
-    store.generation = snap.generation + 1
+  // linear in the number of modifications...
+  reroot(snap.root)
+  store.root = snap.root
+  store.generation = snap.generation + 1
 }
 
 // Common Runtime
@@ -122,96 +122,96 @@ const TOPLEVEL_K = (x, ks) => { throw { computationIsDone: true, result: x } }
 const TOPLEVEL_KS = { prompt: 0, arena: Arena(), rest: null }
 
 function THUNK(f) {
-    f.thunk = true
-    return f
+  f.thunk = true
+  return f
 }
 
 function CAPTURE(body) {
-    return (ks, k) => {
-        const res = body(x => TRAMPOLINE(() => k(x, ks)))
-        if (res instanceof Function) return res
-        else throw { computationIsDone: true, result: $effekt.unit }
-    }
+  return (ks, k) => {
+    const res = body(x => TRAMPOLINE(() => k(x, ks)))
+    if (res instanceof Function) return res
+    else throw { computationIsDone: true, result: $effekt.unit }
+  }
 }
 
 const RETURN = (x, ks) => ks.rest.stack(x, ks.rest)
 
 // HANDLE(ks, ks, (p, ks, k) => { STMT })
 function RESET(prog, ks, k) {
-    const prompt = _prompt++;
-    const rest = { stack: k, prompt: ks.prompt, arena: ks.arena, rest: ks.rest }
-    return prog(prompt, { prompt, arena: Arena([]), rest }, RETURN)
+  const prompt = _prompt++;
+  const rest = { stack: k, prompt: ks.prompt, arena: ks.arena, rest: ks.rest }
+  return prog(prompt, { prompt, arena: Arena([]), rest }, RETURN)
 }
 
 function SHIFT(p, body, ks, k) {
 
-    // TODO avoid constructing this object
-    let meta = { stack: k, prompt: ks.prompt, arena: ks.arena, rest: ks.rest }
-    let cont = null
+  // TODO avoid constructing this object
+  let meta = { stack: k, prompt: ks.prompt, arena: ks.arena, rest: ks.rest }
+  let cont = null
 
-    while (!!meta && meta.prompt !== p) {
-        let store = meta.arena
-        cont = { stack: meta.stack, prompt: meta.prompt, arena: store, backup: snapshot(store), rest: cont }
-        meta = meta.rest
-    }
-    if (!meta) { throw `Prompt not found ${p}` }
-
-    // package the prompt itself
+  while (!!meta && meta.prompt !== p) {
     let store = meta.arena
     cont = { stack: meta.stack, prompt: meta.prompt, arena: store, backup: snapshot(store), rest: cont }
     meta = meta.rest
+  }
+  if (!meta) { throw `Prompt not found ${p}` }
 
-    const k1 = meta.stack
-    meta.stack = null
-    return body(cont, meta, k1)
+  // package the prompt itself
+  let store = meta.arena
+  cont = { stack: meta.stack, prompt: meta.prompt, arena: store, backup: snapshot(store), rest: cont }
+  meta = meta.rest
+
+  const k1 = meta.stack
+  meta.stack = null
+  return body(cont, meta, k1)
 }
 
 // Rewind stack `cont` back onto `k` :: `ks` and resume with c
 function RESUME(cont, c, ks, k) {
-    let meta = { stack: k, prompt: ks.prompt, arena: ks.arena, rest: ks.rest }
-    let toRewind = cont
-    while (!!toRewind) {
-        restore(toRewind.arena, toRewind.backup)
-        meta = { stack: toRewind.stack, prompt: toRewind.prompt, arena: toRewind.arena, rest: meta }
-        toRewind = toRewind.rest
-    }
+  let meta = { stack: k, prompt: ks.prompt, arena: ks.arena, rest: ks.rest }
+  let toRewind = cont
+  while (!!toRewind) {
+    restore(toRewind.arena, toRewind.backup)
+    meta = { stack: toRewind.stack, prompt: toRewind.prompt, arena: toRewind.arena, rest: meta }
+    toRewind = toRewind.rest
+  }
 
-    const k1 = meta.stack // TODO instead copy meta here, like elsewhere?
-    meta.stack = null
-    return () => c(meta, k1)
+  const k1 = meta.stack // TODO instead copy meta here, like elsewhere?
+  meta.stack = null
+  return () => c(meta, k1)
 }
 
 function RUN_TOPLEVEL(comp) {
-    try {
-        let a = comp(TOPLEVEL_KS, TOPLEVEL_K)
-        while (true) { a = a() }
-    } catch (e) {
-        if (e.computationIsDone) return e.result
-        else throw e
-    }
+  try {
+    let a = comp(TOPLEVEL_KS, TOPLEVEL_K)
+    while (true) { a = a() }
+  } catch (e) {
+    if (e.computationIsDone) return e.result
+    else throw e
+  }
 }
 
 // trampolines the given computation (like RUN_TOPLEVEL, but doesn't provide continuations)
 function TRAMPOLINE(comp) {
-    let a = comp;
-    try {
-        while (true) {
-            a = a()
-        }
-    } catch (e) {
-        if (e.computationIsDone) return e.result
-        else throw e
+  let a = comp;
+  try {
+    while (true) {
+      a = a()
     }
+  } catch (e) {
+    if (e.computationIsDone) return e.result
+    else throw e
+  }
 }
 
 // keeps the current trampoline going and dispatches the given task
 function RUN(task) {
-    return () => task(TOPLEVEL_KS, TOPLEVEL_K)
+  return () => task(TOPLEVEL_KS, TOPLEVEL_K)
 }
 
 // aborts the current continuation
 function ABORT(value) {
-    throw { computationIsDone: true, result: value }
+  throw { computationIsDone: true, result: value }
 }
 
 
@@ -264,59 +264,59 @@ $effekt.runToplevel = RUN_TOPLEVEL
 
 
 $effekt.show = function(obj) {
-    if (!!obj && !!obj.__reflect) {
-        const meta = obj.__reflect()
-        return meta.__name + "(" + meta.__data.map($effekt.show).join(", ") + ")"
-    }
-    else if (!!obj && obj.__unit) {
-        return "()";
-    } else {
-        return "" + obj;
-    }
+  if (!!obj && !!obj.__reflect) {
+    const meta = obj.__reflect()
+    return meta.__name + "(" + meta.__data.map($effekt.show).join(", ") + ")"
+  }
+  else if (!!obj && obj.__unit) {
+    return "()";
+  } else {
+    return "" + obj;
+  }
 }
 
 $effekt.equals = function(obj1, obj2) {
-    if (!!obj1.__equals) {
-        return obj1.__equals(obj2)
-    } else {
-        return (obj1.__unit && obj2.__unit) || (obj1 === obj2);
-    }
+  if (!!obj1.__equals) {
+    return obj1.__equals(obj2)
+  } else {
+    return (obj1.__unit && obj2.__unit) || (obj1 === obj2);
+  }
 }
 
 function compare$prim(n1, n2) {
-    if (n1 == n2) { return 0; }
-    else if (n1 > n2) { return 1; }
-    else { return -1; }
+  if (n1 == n2) { return 0; }
+  else if (n1 > n2) { return 1; }
+  else { return -1; }
 }
 
 $effekt.compare = function(obj1, obj2) {
-    if ($effekt.equals(obj1, obj2)) { return 0; }
+  if ($effekt.equals(obj1, obj2)) { return 0; }
 
-    if (!!obj1 && !!obj2) {
-        if (!!obj1.__reflect && !!obj2.__reflect) {
-            const tagOrdering = compare$prim(obj1.__tag, obj2.__tag)
-            if (tagOrdering != 0) { return tagOrdering; }
+  if (!!obj1 && !!obj2) {
+    if (!!obj1.__reflect && !!obj2.__reflect) {
+      const tagOrdering = compare$prim(obj1.__tag, obj2.__tag)
+      if (tagOrdering != 0) { return tagOrdering; }
 
-            const meta1 = obj1.__reflect().__data
-            const meta2 = obj2.__reflect().__data
+      const meta1 = obj1.__reflect().__data
+      const meta2 = obj2.__reflect().__data
 
-            const lengthOrdering = compare$prim(meta1.length, meta2.length)
-            if (lengthOrdering != 0) { return lengthOrdering; }
+      const lengthOrdering = compare$prim(meta1.length, meta2.length)
+      if (lengthOrdering != 0) { return lengthOrdering; }
 
-            for (let i = 0; i < meta1.length; i++) {
-                const contentOrdering = $effekt.compare(meta1[i], meta2[i])
-                if (contentOrdering != 0) { return contentOrdering; }
-            }
+      for (let i = 0; i < meta1.length; i++) {
+        const contentOrdering = $effekt.compare(meta1[i], meta2[i])
+        if (contentOrdering != 0) { return contentOrdering; }
+      }
 
-            return 0;
-        }
+      return 0;
     }
+  }
 
-    return compare$prim(obj1, obj2);
+  return compare$prim(obj1, obj2);
 }
 
 $effekt.println = function println$impl(str) {
-    console.log(str); return $effekt.unit;
+  console.log(str); return $effekt.unit;
 }
 
 $effekt.unit = { __unit: true }
@@ -328,57 +328,57 @@ $effekt.hole = function(pos) { throw pos + " not implemented yet" }
 
 
 $effekt.readln = function readln$impl(callback) {
-    const readline = require('node:readline').createInterface({
-        input: process.stdin,
-        output: process.stdout,
-    });
-    readline.question('', (answer) => {
-        readline.close();
-        callback(answer);
-    });
+  const readline = require('node:readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  readline.question('', (answer) => {
+    readline.close();
+    callback(answer);
+  });
 }
 
 
 
-function array$set(arr, index, value) {
+  function array$set(arr, index, value) {
     arr[index] = value;
     return $effekt.unit
-}
+  }
 
 
 
-function bytearray$set(bytes, index, value) {
+  function bytearray$set(bytes, index, value) {
     bytes[index] = value;
     return $effekt.unit;
-}
+  }
 
-function bytearray$compare(arr1, arr2) {
+  function bytearray$compare(arr1, arr2) {
     const len = Math.min(arr1.length, arr2.length);
 
     for (let i = 0; i < len; i++) {
-        if (arr1[i] !== arr2[i]) {
-            return arr1[i] < arr2[i] ? -1 : 1;
-        }
+      if (arr1[i] !== arr2[i]) {
+        return arr1[i] < arr2[i] ? -1 : 1;
+      }
     }
 
     if (arr1.length !== arr2.length) {
-        return arr1.length < arr2.length ? -1 : 1;
+      return arr1.length < arr2.length ? -1 : 1;
     } else {
-        return 0;
+      return 0;
     }
-}
+  }
 
 
 
-function set$impl(ref, value) {
+  function set$impl(ref, value) {
     ref.value = value;
     return $effekt.unit;
-}
+  }
 
 
 function inspect_0(value_0, ks_0, k_0) {
-    const v_r_0 = $effekt.println(($effekt.show(value_0)));
-    return () => k_0(v_r_0, ks_0);
+  const v_r_0 = $effekt.println(($effekt.show(value_0)));
+  return () => k_0(v_r_0, ks_0);
 }
 
 import {emptyContext, HoleContext} from "./HoleContext.js";
