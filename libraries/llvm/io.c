de54ae18305bde01353cd1bbd5664bdc4ecbb2ae
Marcial Gaißert <uni@gaisseml.de>
2025-11-10 15:53:02 +0100

    Free chunk

--- libraries/llvm/io.c
+++ libraries/llvm/io.c
@@ -752,6 +752,7 @@ void subproc_stream_cb(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) 
   subproc_stream_cb_closure_t* clos = (subproc_stream_cb_closure_t*)(stream->data);
   if(nread >= 0) {
     struct Pos chunk = c_bytearray_construct(nread, (uint8_t*)(buf->base));
+    if (buf->base) free(buf->base);
     run_Pos(clos->handler, chunk);
   } else {
     uv_read_stop(stream);
