1210bb279a1244e2b29b6b12bbd6584305cc81d7
Jiří Beneš <mail@jiribenes.com>
2025-11-11 21:10:27 +0100

    Fix dummy unifier implementation, add tests

--- effekt/shared/src/main/scala/effekt/typer/CapabilityScope.scala
+++ effekt/shared/src/main/scala/effekt/typer/CapabilityScope.scala
@@ -2,7 +2,7 @@ package effekt
 package typer
 
 import effekt.context.Context
-import effekt.symbols.{Captures, Type, TypeVar, ValueType}
+import effekt.symbols.{BlockType, Captures, Type, TypeVar, ValueType}
 import effekt.{source, symbols}
 import effekt.util.messages.ErrorMessageReifier
 
@@ -21,20 +21,20 @@ case object GlobalCapabilityScope extends CapabilityScope {
   // potentially handle it, then we raise an error.
   def capabilityFor(tpe: symbols.InterfaceType)(using C: Context): symbols.BlockParam =
     C.abort(pretty"Effect ${tpe} is not allowed in this context.")
+  def relevantInScopeFor(tpe: BlockType.InterfaceType)(using C: Context): Set[BlockType.InterfaceType] = Set.empty
 }
 
 object DummyUnifier extends TypeUnifier {
-  var hasError = false
+  // XXX(HACK): Side-channeling the output of `unifyValueTypes` via this variable
+  var success = true
 
   def checkSubtype(t1: effekt.symbols.ValueType, t2: effekt.symbols.ValueType)(using C: Context): Boolean = {
-    hasError = false
+    success = true
     unifyValueTypes(
       C.substitution.substitute(t1),
       C.substitution.substitute(t2),
       ErrorContext.MergeTypes(t1, t2))
-    val hadError = hasError
-    hasError = false
-    hadError
+    success
   }
 
   override def requireLowerBound(x: TypeVar.UnificationVar, tpe: ValueType, ctx: ErrorContext): Unit = ()
@@ -42,31 +42,34 @@ object DummyUnifier extends TypeUnifier {
   override def requireEqual(x: TypeVar.UnificationVar, tpe: ValueType, ctx: ErrorContext): Unit = ()
   override def requireSubregion(lower: Captures, upper: Captures, ctx: ErrorContext): Unit = ()
 
-  override def abort(msg: String, ctx: ErrorContext): Nothing = ??? // shouldn't ever be triggered [?]
+  override def abort(msg: String, ctx: ErrorContext): Nothing = sys error "Unexpected abort in DummyUnifier!" // NOTE(jiribenes, 2025-11-11): shouldn't ever be triggered!
 
-  override def error(msg: String, ctx: ErrorContext): Unit = { hasError = true }
-  override def error(left: Type, right: Type, ctx: ErrorContext): Unit = { hasError = true }
+  override def error(msg: String, ctx: ErrorContext): Unit = { success = false }
+  override def error(left: Type, right: Type, ctx: ErrorContext): Unit = { success = false }
 }
 
 class BindSome(binder: source.Tree, capabilities: Map[symbols.InterfaceType, symbols.BlockParam],val parent: CapabilityScope) extends CapabilityScope {
   def copy: CapabilityScope = BindSome(binder, capabilities, parent.copy)
   def capabilityFor(tpe: symbols.InterfaceType)(using C: Context): symbols.BlockParam =
     capabilities.get(tpe).orElse {
-      // If the requested type has unbound type parameters, try lexical resolution
+      // If the requested type has unbound type parameters, try lexical resolution (see PR #1194)
       unknowns(tpe).toList match {
         case Nil => None
-        case _ =>
+        case _ => {
           def isValidFit(handlerTpe: symbols.InterfaceType): Boolean =
             handlerTpe.typeConstructor == tpe.typeConstructor &&
               (tpe.args zip handlerTpe.args).forall { case (inferred, concrete) => DummyUnifier.checkSubtype(inferred, concrete) }
 
           capabilities.collect { case (handlerTpe, cap) if isValidFit(handlerTpe) => (handlerTpe, cap) }.toList match {
-            case Nil => None
             case List((handlerTpe, cap)) =>
-              (tpe.args zip handlerTpe.args).foreach { case (inferred, concrete) => C.requireSubtype(inferred, concrete, ErrorContext.MergeTypes(inferred, concrete)) }
+              (tpe.args zip handlerTpe.args).foreach { case (inferred, concrete) =>
+                C.requireSubtype(inferred, concrete, ErrorContext.MergeTypes(inferred, concrete))
+              }
               Some(cap)
-            case _ => None // TODO: Could we have a better error message here?
+            case Nil => None
+            case caps => None
           }
+        }
       }
     }.getOrElse(parent.capabilityFor(tpe))
   override def toString: String = s"BindSome(${binder.getClass.getSimpleName}, ${capabilities}, ${parent})"
