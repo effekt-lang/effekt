d041ae7a4dd8c8028aeea3a276b32347826aa8c6
Marcial Gaißert <github@gaisseml.de>
2025-11-11 12:37:30 +0100

    LLVM: Fix buffering of stdout (#1198)
    According to https://stackoverflow.com/a/4201325 , stdout is not
    (necessarily) line-buffered when going to a non-interactive output,
    which sbt probably is (from the point of view of the compiled Effekt
    program). So *all* output in this case is fully buffered.
    
    Here, we explicitly set `stdout` to be line buffered in `main` (using
    `setvbuf`).
    
    This fixes #1087 ~(at least for me, but writing an automated test for
    this would need changes in the test framework AFAICT)~.
    Now includes a test case using filesystem to write to /dev/stdout in
    between.
    
    ### Alternatives
    - Manually `fflush` after every println (see 08f952f / 1e6b928)

--- libraries/llvm/main.c
+++ libraries/llvm/main.c
@@ -34,6 +34,7 @@ uint64_t c_get_argc() {
 int main(int argc, char *argv[]) {
     program_argc = argc;
     program_argv = argv;
+    setvbuf(stdout, NULL, _IOLBF, BUFSIZ);
     effektMain();
     uv_loop_t *loop = uv_default_loop();
     uv_run(loop, UV_RUN_DEFAULT);
