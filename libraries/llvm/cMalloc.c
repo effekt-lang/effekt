eb5f49b028adb045b75d778bb9b60eff94e02598
Flat <daniel.flat@wps.de>
2025-11-14 10:36:59 +0100

    speedup of flushing todo-list approach by implementing a usedBitMask which stores the index of the block as bits with 1 = used, 0 = not in usage

--- libraries/llvm/cMalloc.c
+++ libraries/llvm/cMalloc.c
@@ -30,13 +30,39 @@ typedef struct Block
 
 const bool DEBUG = false;
 static Block* todoList = SENTINEL_BLOCK; // Head of the To-Do-List
-static Block* usedList = SENTINEL_BLOCK; // Head of the Used-List
+static uint8_t* usedBitMask;
+static size_t bitsetSize;
 static uint8_t* nextUnusedBlock = NULL; // Pointer to the next unused Block
 static uint8_t* endOfChunk = NULL; // End of the allocated Storage
 static const int blockSize = 64; // The size of each block (64B)
 
 // How much storage do we allocate at the beginning of a program? =4GB
 static const size_t chunkSize = (size_t)4294967296ULL;
+
+// Berechnet den Block-Index zu einer gegebenen Pointer‐Adresse.
+// Dazu wird die Adresse in einen Abstand zum Chunk‐Anfang umgerechnet
+// und anschließend durch die Blockgröße geteilt.
+static inline size_t blockIndexFromPtr(const void* ptr) {
+    return ((uint8_t*)ptr - (endOfChunk - chunkSize)) / blockSize;
+}
+
+// Markiert einen Block als „benutzt“, indem im Bitset das passende Bit
+// gesetzt wird. index >> 3 wählt das Byte, index & 7 wählt das Bit darin.
+static inline void usedSet(size_t index) {
+    usedBitMask[index >> 3] |=  (1 << (index & 7));
+}
+
+// Markiert einen Block als „frei“, indem im Bitset das passende Bit
+// gelöscht (auf 0 gesetzt) wird.
+static inline void usedClear(size_t index) {
+    usedBitMask[index >> 3] &= ~(1 << (index & 7));
+}
+
+// Prüft, ob ein bestimmter Block genutzt ist. Schiebt das relevante Bit
+// auf Position 0 und liest dann aus, ob es 1 oder 0 ist.
+static inline bool usedTest(size_t index) {
+    return (usedBitMask[index >> 3] >> (index & 7)) & 1;
+}
 
 void printTodoList(const Block *todoList) {
     printf("All Elements in Todo List:\n");
@@ -64,8 +90,23 @@ void cInitializeMemory()
 
     nextUnusedBlock = mem;
     endOfChunk = mem + chunkSize;
+
+    // -----------------------------
+    // Bitset für alle 4GB Blöcke bauen
+    // -----------------------------
+    size_t numberOfSlots = chunkSize / blockSize;
+    size_t requiredBytesForRepresentingEachSlot = (numberOfSlots + 7) / 8;
+    bitsetSize = requiredBytesForRepresentingEachSlot * 8;
+
+    usedBitMask = calloc(requiredBytesForRepresentingEachSlot, 1);
+    if (!usedBitMask) {
+        printf("Error: bitset alloc failed!\n");
+        exit(1);
+    }
+
     if (DEBUG) {
         printf("[init] Memory initialized: %p - %p\n", (void*)mem, (void*)endOfChunk);
+        printf("[init] Bitset: %zu bytes for %zu blocks\n", requiredBytesForRepresentingEachSlot, numberOfSlots);    // we require 8MB to store the bitmask
     }
 }
 
@@ -95,18 +136,30 @@ void* acquire(uint8_t size)
         if (DEBUG) {
             printf("[acquire] New block: %p\n", block);
         }
+
+        // Markieren des Blocks als frei
+        size_t idx = blockIndexFromPtr(block);
+//        printf("idx: %zu\n", idx);
+        usedSet(idx);
+
         return block;
     }
 
     // 2. Falls Todolist nicht leer ist → wiederverwenden
     Block* block = todoList;
+    todoList = block->next;
 
     if (DEBUG) {
         printf("[acquire] Reusing block: %p\n", (void*)block);
     }
 
-    todoList = block->next;
+    // 3. Wir müssen die Children von dem Block erasen, um den Block neu befüllen zu können.
     block->eraser((void*)block);
+
+    // 4. Markieren des Blocks als verwendet
+    size_t idx = blockIndexFromPtr(block);
+//    printf("idx: %zu\n", idx);
+    usedSet(idx);
 
     return (void*)block;
 }
@@ -131,47 +184,48 @@ void release(void* ptr)
     Block* block = (Block*)ptr;
     block->next = todoList;
     todoList = block;
+
+    // Markieren des Blocks als frei
+    size_t idx = blockIndexFromPtr(block);
+//    printf("idx: %zu\n", idx);
+    usedClear(idx);
 }
 
 /**
-*
+*   Checks if we have leaked blocks. A block is leaked, if it is not in the to-do-list.
+*    If we have leaked blocks, we flush all of them to our to-do list.
 */
 void assertNumberLeakedBlocks(int expected) {
-    // Calculate the number of leaked blocks
+    // Total number of blocks that were ever allocated
+    uint8_t* firstSlot = (endOfChunk - chunkSize); // the start of the chunk
+    const size_t totalAllocated = (nextUnusedBlock - firstSlot) / blockSize;
+
     size_t numberOfElementsInTodoList = 0;
     for (const Block* b = todoList; b != SENTINEL_BLOCK; b = b->next) {
         numberOfElementsInTodoList++;
     }
+    size_t numberOfLeakedBlocks = 0;
 
-    // Total number of blocks that were ever allocated
-    const uint8_t* firstBlock = (endOfChunk - chunkSize); // the start of the chunk
-    const size_t totalAllocated = (nextUnusedBlock - firstBlock) / blockSize;
+//    printf("requiredBytesForRepresentingEachSlot: %zu\n", bitsetSize);
 
-    // Calculate the number of leaked blocks
-    const size_t numberOfLeakedBlocks = totalAllocated - numberOfElementsInTodoList;
+    // Flush: turn all "used" (bit=1) blocks into todoList
+    for (uint8_t* ptr = (uint8_t*)firstSlot; ptr < nextUnusedBlock; ptr+= blockSize) {
+//        printf("block: %p\n", ptr);
+        size_t idx = blockIndexFromPtr(ptr);
+//        printf("idx: %zu\n", idx);
+        bool isUsed = usedTest(idx);
+//        printf("isUsed: %d\n", isUsed);
+        if (isUsed) {
+            release((Block*)ptr);
+            numberOfLeakedBlocks++;
+        }
+    }
 
-    // if there leakes slots...
-//    if (numberOfLeakedBlocks != expected) {
-//        exit(1);
-////        // we traverse each slot of the to-do-list and add it to the to-do list if missing
-////        size_t numberOfSlotsToFlush = numberOfLeakedBlocks;
-////        uint8_t* slotToAnalyze = (endOfChunk - chunkSize); // the start of the chunk
-////        while(numberOfSlotsToFlush != expected) {
-////            Block* blockToFlush = (Block*) slotToAnalyze;
-////            bool isMissing = true;
-////            for (const Block* b = todoList; b != NULL; b = b->next) {
-////                if ((void*)b == (void*)blockToFlush) {
-////                    isMissing = false;
-////                    break;
-////                }
-////            }
-////            if (isMissing) {
-////                release(blockToFlush);
-////                numberOfSlotsToFlush--;
-////            }
-////            slotToAnalyze = slotToAnalyze + blockSize;
-////        }
-//    }
+    // Assert that the number of leaked blocks is as expected
+    if ((totalAllocated - numberOfElementsInTodoList - numberOfLeakedBlocks) != expected) {
+        printf("Totally allocated: %zu | numberOfElementsInTodoList %zu | numberOfLeakedBlocks %zu \n", totalAllocated, numberOfElementsInTodoList, numberOfLeakedBlocks);
+        exit(1);
+    }
 }
 
 // If there are any open libuv handles or requests, this function will block until they are closed.
@@ -198,7 +252,7 @@ void testIfAllBlocksAreFreed()
 {
     assertThatAllAsynchronousOperationsAreFinished();   // closing all open handles
     assertLeakFree();                  // testing malloc & calloc & free
-    assertNumberLeakedBlocks(0);    // testing aquire & release
+    assertNumberLeakedBlocks(0);    // testing acquire & release
 }
 
 // small testprogram to test the allocator
