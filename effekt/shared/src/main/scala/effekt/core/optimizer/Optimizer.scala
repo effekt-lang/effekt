f78d52804019c8a67fad0b073bf290b30d87da1b
dvdvgt <40773635+dvdvgt@users.noreply.github.com>
2025-11-18 10:05:23 +0100

    use old optimization pipeline with new-normalizer

--- effekt/shared/src/main/scala/effekt/core/optimizer/Optimizer.scala
+++ effekt/shared/src/main/scala/effekt/core/optimizer/Optimizer.scala
@@ -30,61 +30,23 @@ object Optimizer extends Phase[CoreTransformed, CoreTransformed] {
 
     if !Context.config.optimize() then return tree;
 
-    /*
-    def inlineSmall(usage: Map[Id, Usage]) = NewNormalizer { (id, b) =>
-      usage.get(id).contains(Once) || (!usage.get(id).contains(Recursive) && b.size < 40)
-    }
-    val dontInline = NewNormalizer { (id, b) => false }
-    def inlineUnique(usage: Map[Id, Usage]) = NewNormalizer { (id, b) => usage.get(id).contains(Once) }
-    def inlineAll(usage: Map[Id, Usage]) = NewNormalizer { (id, b) => !usage.get(id).contains(Recursive) }
-    */
-    // tree = Context.timed("new-normalizer-1", source.name) { inlineSmall(Reachable(Set(mainSymbol), tree)).run(tree) }
+    // (2) lift static arguments
     tree = StaticArguments.transform(mainSymbol, tree)
-    //util.trace(util.show(tree))
-    tree = Context.timed("new-normalizer-1", source.name) { NewNormalizer().run(tree) }
-    //util.trace(util.show(tree))
+
     val inliningPolicy = UniqueJumpSimple(
       maxInlineSize = 150
     )
-    val reachability = Reachable(Set(mainSymbol), tree)
-    //util.trace(util.show(tree))
-    tree = Inliner(inliningPolicy, reachability).run(tree)
-    tree = Context.timed("new-normalizer-1", source.name) { NewNormalizer().run(tree) }
-    //util.trace(util.show(tree))
-    tree = Deadcode.remove(mainSymbol, tree)
-    util.trace(util.show(tree))
+    def normalize(m: ModuleDecl) = {
+      val anfed = BindSubexpressions.transform(m)
+      val normalized = NewNormalizer().run(anfed)
+      val inlined = Inliner(inliningPolicy, Reachable(Set(mainSymbol), normalized)).run(normalized)
+      val live = Deadcode.remove(mainSymbol, inlined)
+      val tailRemoved = RemoveTailResumptions(live)
+      val contified = DirectStyle.rewrite(tailRemoved)
+      contified
+    }
 
-    //    Normalizer.assertNormal(tree)
-    //tree = Normalizer.normalize(Set(mainSymbol), tree, Context.config.maxInlineSize().toInt)
-
-    //    tree = Context.timed("old-normalizer-1", source.name) { Normalizer.normalize(Set(mainSymbol), tree, 0) }
-    //    tree = Context.timed("old-normalizer-2", source.name) { Normalizer.normalize(Set(mainSymbol), tree, 0) }
-    //
-    //    tree = Context.timed("new-normalizer-3", source.name) { NewNormalizer.run(tree) }
-    //    Normalizer.assertNormal(tree)
-
-    // (2) lift static arguments
-    //    tree = Context.timed("static-argument-transformation", source.name) {
-    //      StaticArguments.transform(mainSymbol, tree)
-    //    }
-    //
-    //    tree = Context.timed("new-normalizer-3", source.name) { NewNormalizer.run(tree) }
-    //    Normalizer.assertNormal(tree)
-    //
-    //    def normalize(m: ModuleDecl) = {
-    //      val anfed = BindSubexpressions.transform(m)
-    //      val normalized = Normalizer.normalize(Set(mainSymbol), anfed, Context.config.maxInlineSize().toInt)
-    //      Normalizer.assertNormal(normalized)
-    //      val live = Deadcode.remove(mainSymbol, normalized)
-    //      val tailRemoved = RemoveTailResumptions(live)
-    //      val contified = DirectStyle.rewrite(tailRemoved)
-    //      contified
-    //    }
-    //
-    //    // (3) normalize a few times (since tail resumptions might only surface after normalization and leave dead Resets)
-    //    tree = Context.timed("normalize-1", source.name) { normalize(tree) }
-    //    tree = Context.timed("normalize-2", source.name) { normalize(tree) }
-    //    tree = Context.timed("normalize-3", source.name) { normalize(tree) }
-
+    tree = Context.timed("new-normalizer-1", source.name) { normalize(tree) }
+    tree = Context.timed("new-normalizer-2", source.name) { normalize(tree) }
     tree
 }
