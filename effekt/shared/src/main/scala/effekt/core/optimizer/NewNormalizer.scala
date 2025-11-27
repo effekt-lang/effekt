5794b869828609805d7027309b42e691d7f2d9a1
Tim Süberkrüb <dev@timsueberkrueb.io>
2025-11-27 10:04:12 +0100

    Add further builtin integer operations from vm builtins

--- effekt/shared/src/main/scala/effekt/core/optimizer/NewNormalizer.scala
+++ effekt/shared/src/main/scala/effekt/core/optimizer/NewNormalizer.scala
@@ -1515,7 +1515,7 @@ def builtin(name: String)(impl: List[semantics.Value] ~> semantics.Value): (Stri
 type Builtins = Map[String, BuiltinImpl]
 
 lazy val integers: Builtins = Map(
-  // Arithmetic
+  // Integer arithmetic operations with symbolic simplification support
   // ----------
   builtin("effekt::infixAdd(Int, Int)") {
     case As.IntExpr(x) :: As.IntExpr(y) :: Nil => semantics.Value.Integer(theories.Integers.add(x, y))
@@ -1526,12 +1526,58 @@ lazy val integers: Builtins = Map(
   builtin("effekt::infixMul(Int, Int)") {
     case As.IntExpr(x) :: As.IntExpr(y) :: Nil => semantics.Value.Integer(theories.Integers.mul(x, y))
   },
+  // Integer arithmetic operations only evaluated for literals
+  // ----------
   builtin("effekt::infixDiv(Int, Int)") {
     case As.Int(x) :: As.Int(y) :: Nil if y != 0 => semantics.Value.Integer(theories.Integers.embed(x / y))
   },
   builtin("effekt::mod(Int, Int)") {
     case As.Int(x) :: As.Int(y) :: Nil if y != 0 => semantics.Value.Integer(theories.Integers.embed(x % y))
-  }
+  },
+  builtin("effekt::bitwiseShl(Int, Int)") {
+    case As.Int(x) :: As.Int(y) :: Nil => semantics.Value.Integer(theories.Integers.embed(x << y))
+  },
+  builtin("effekt::bitwiseShr(Int, Int)") {
+    case As.Int(x) :: As.Int(y) :: Nil => semantics.Value.Integer(theories.Integers.embed(x >> y))
+  },
+  builtin("effekt::bitwiseAnd(Int, Int)") {
+    case As.Int(x) :: As.Int(y) :: Nil => semantics.Value.Integer(theories.Integers.embed(x & y))
+  },
+  builtin("effekt::bitwiseOr(Int, Int)") {
+    case As.Int(x) :: As.Int(y) :: Nil => semantics.Value.Integer(theories.Integers.embed(x | y))
+  },
+  builtin("effekt::bitwiseXor(Int, Int)") {
+    case As.Int(x) :: As.Int(y) :: Nil => semantics.Value.Integer(theories.Integers.embed(x ^ y))
+  },
+  // Comparison
+  // ----------
+  builtin("effekt::infixEq(Int, Int)") {
+    case As.Int(x) :: As.Int(y) :: Nil => semantics.Value.Literal(x == y, Type.TBoolean)
+  },
+  builtin("effekt::infixNeq(Int, Int)") {
+    case As.Int(x) :: As.Int(y) :: Nil => semantics.Value.Literal(x != y, Type.TBoolean)
+  },
+  builtin("effekt::infixLt(Int, Int)") {
+    case As.Int(x) :: As.Int(y) :: Nil => semantics.Value.Literal(x < y, Type.TBoolean)
+  },
+  builtin("effekt::infixGt(Int, Int)") {
+    case As.Int(x) :: As.Int(y) :: Nil => semantics.Value.Literal(x > y, Type.TBoolean)
+  },
+  builtin("effekt::infixLte(Int, Int)") {
+    case As.Int(x) :: As.Int(y) :: Nil => semantics.Value.Literal(x <= y, Type.TBoolean)
+  },
+  builtin("effekt::infixGte(Int, Int)") {
+    case As.Int(x) :: As.Int(y) :: Nil => semantics.Value.Literal(x >= y, Type.TBoolean)
+  },
+  // Conversion
+  // ----------
+  builtin("effekt::toDouble(Int)") {
+    case As.Int(x) :: Nil => semantics.Value.Literal(x.toDouble, Type.TDouble)
+  },
+
+  builtin("effekt::show(Int)") {
+    case As.Int(n) :: Nil => semantics.Value.Literal(n.toString, Type.TString)
+  },
 )
 
 lazy val supportedBuiltins: Builtins = integers
