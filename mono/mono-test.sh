1742661c52d2f61b4b3ecfe31e3aaa9e5d039998
Mattis Böckle <mattisderbock@gmail.com>
2025-12-21 17:13:37 +0100

    Fix old examples

--- mono/mono-test.sh
+++ mono/mono-test.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env zsh
 sbt "project effektJVM" \
-	"run mono/mono1.effekt" \
-	"run mono/mono2.effekt" \
-	"run mono/mono3.effekt" \
-	"run mono/mono4.effekt" \
-	"run mono/mono5.effekt" \
-	"run mono/mono6.effekt" \
-	"run mono/mono7.effekt" \
-	"run mono/mono8.effekt" \
-	"run mono/mono9.effekt" \
-	"run mono/mono10.effekt" \
-	"run mono/mono11.effekt" \
-	"run mono/mono12.effekt" \
-	"run mono/mono13.effekt" \
+	"run --backend llvm mono/mono1.effekt" \
+	"run --backend llvm mono/mono2.effekt" \
+	"run --backend llvm mono/mono3.effekt" \
+	"run --backend llvm mono/mono4.effekt" \
+	"run --backend llvm mono/mono5.effekt" \
+	"run --backend llvm mono/mono6.effekt" \
+	"run --backend llvm mono/mono7.effekt" \
+	"run --backend llvm mono/mono8.effekt" \
+	"run --backend llvm mono/mono9.effekt" \
+	"run --backend llvm mono/mono10.effekt" \
+	"run --backend llvm mono/mono11.effekt" \
+	"run --backend llvm mono/mono12.effekt" \
+	"run --backend llvm mono/mono13.effekt" \
+	"run --backend llvm mono/mono14_1.effekt" \
+	"run --backend llvm mono/mono14_2.effekt" \
+	"run --backend llvm mono/mono15.effekt" \
+	"run --backend llvm mono/mono16.effekt" \
