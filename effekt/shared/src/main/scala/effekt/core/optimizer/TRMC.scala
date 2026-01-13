eb93db8f573973902dee35d2b6746d209858fbdf
Heinrich Timphus <heinrich.timphus@web.de>
2026-01-13 14:17:27 +0100

    start tests for JS, start phase TRMC

---
+++ effekt/shared/src/main/scala/effekt/core/optimizer/TRMC.scala
@@ -0,0 +1,23 @@
+package effekt.core.optimizer
+
+import effekt.{Phase, PhaseResult}
+import effekt.PhaseResult.{CoreTransformed, Typechecked}
+import effekt.context.Context
+import effekt.core.{ModuleDecl, Tree}
+
+object TRMC extends Phase[CoreTransformed, CoreTransformed]{
+  val phaseName: String = "trmc"
+
+  def run(input: CoreTransformed)(using Context): Option[CoreTransformed] =
+    val CoreTransformed(source, tree, mod, modDec) = input
+
+    val transformed = Context.timed(phaseName, source.name) {
+      trmc(modDec)
+    }
+
+    Some(CoreTransformed(source, tree, mod, transformed))
+  def trmc(tree: ModuleDecl): ModuleDecl = tree match {
+    case 
+  }
+  
+}
