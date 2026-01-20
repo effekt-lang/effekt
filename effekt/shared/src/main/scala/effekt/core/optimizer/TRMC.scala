b7978b145b5aa9d7d5d70b178d012c619138a30e
Heinrich Timphus <heinrich.timphus@web.de>
2026-01-20 12:14:30 +0100

    start phase TRMC, extern defs for Contexts

--- effekt/shared/src/main/scala/effekt/core/optimizer/TRMC.scala
+++ effekt/shared/src/main/scala/effekt/core/optimizer/TRMC.scala
@@ -3,20 +3,32 @@ package effekt.core.optimizer
 import effekt.{Phase, PhaseResult}
 import effekt.PhaseResult.{CoreTransformed, Typechecked}
 import effekt.context.Context
-import effekt.core.{ModuleDecl, Tree}
+import effekt.core.Stmt.App
+import effekt.core.{Block, Expr, ModuleDecl, Stmt, Tree, ValueType}
 
 object TRMC extends Phase[CoreTransformed, CoreTransformed]{
   val phaseName: String = "trmc"
 
   def run(input: CoreTransformed)(using Context): Option[CoreTransformed] =
     val CoreTransformed(source, tree, mod, modDec) = input
+    
+    object transform extends Tree.Rewrite{
+      override def stmt: PartialFunction[Stmt, Stmt] = {
+        case Stmt.App(callee, targs, vargs, bargs) if tailRecModCons(callee, targs, vargs, bargs) => 
+          trmc(callee, targs, vargs, bargs)
+      }
+    }
 
     val transformed = Context.timed(phaseName, source.name) {
-      trmc(modDec)
+      transform.rewrite(modDec)
     }
 
     Some(CoreTransformed(source, tree, mod, transformed))
-  def trmc(tree: ModuleDecl): ModuleDecl =
-    tree
+    
+  def tailRecModCons(callee: Block, targs: List[ValueType], vargs: List[Expr], bargs: List[Block]): Boolean =
+    false
+  
+  def trmc(callee: Block, targs: List[ValueType], vargs: List[Expr], bargs: List[Block]): Stmt =
+    App(callee: Block, targs: List[ValueType], vargs: List[Expr], bargs: List[Block])
   
 }
