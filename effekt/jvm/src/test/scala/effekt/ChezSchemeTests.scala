cf6e906531bc8befd970896f6d3e415aca11c392
Philipp Schuster <philipp.schuster@uni-tuebingen.de>
2026-03-25 11:28:07 +0100

    More benchmarks for tasks and channels (#1286)
    This benchmark program is faster than the equivalent one in other
    languages for small inputs when compiled with the llvm backend, but much
    much slower for large inputs. We suspect there to be a performance bug
    that makes it have the wrong time complexity. One should investigate,
    for example by plotting the timing for different input sizes, and
    comparing to other backends.

--- effekt/jvm/src/test/scala/effekt/ChezSchemeTests.scala
+++ effekt/jvm/src/test/scala/effekt/ChezSchemeTests.scala
@@ -41,6 +41,8 @@ abstract class ChezSchemeTests extends EffektTests {
     examplesDir / "benchmarks" / "input_output" / "financial_format.effekt",
     examplesDir / "benchmarks" / "input_output" / "server_client.effekt",
     examplesDir / "benchmarks" / "input_output" / "sender_receiver.effekt",
+    examplesDir / "benchmarks" / "input_output" / "channel_primes.effekt",
+    examplesDir / "benchmarks" / "input_output" / "thread_ring.effekt",
     examplesDir / "pos" / "issue1087.effekt",
 
     // unsafe continuations are not yet supported in our Chez backend
@@ -106,6 +108,8 @@ class ChezSchemeCPSTests extends EffektTests {
     examplesDir / "benchmarks" / "input_output" / "financial_format.effekt",
     examplesDir / "benchmarks" / "input_output" / "server_client.effekt",
     examplesDir / "benchmarks" / "input_output" / "sender_receiver.effekt",
+    examplesDir / "benchmarks" / "input_output" / "channel_primes.effekt",
+    examplesDir / "benchmarks" / "input_output" / "thread_ring.effekt",
     examplesDir / "pos" / "issue1087.effekt",
 
     // the test is specific to JS
