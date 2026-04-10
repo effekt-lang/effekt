2ced9dd7091d11a12499e09c9d40019c7e91618e
Heinrich Timphus <heinrich.timphus@web.de>
2026-04-10 17:39:25 +0200

    Add Externs to mock Decleration Contexts, add Types to HoleContext

--- effekt/jvm/src/test/scala/effekt/core/TRMCTests.scala
+++ effekt/jvm/src/test/scala/effekt/core/TRMCTests.scala
@@ -2,6 +2,8 @@ package effekt
 package core
 
 import effekt.core.BlockType.Function
+import effekt.core.ExternBody.StringExternBody
+import effekt.source.FeatureFlag.Default
 import effekt.symbols.Capture
 import munit.Clue.generate
 
@@ -27,8 +29,25 @@ class TRMCTests extends CoreTests {
       Field(tailId, TList)
     ))
   ))
+  val holeCtxId = Id("HoleContext")
+  def HoleContext(a: ValueType, b: ValueType): ValueType = ValueType.Data(holeCtxId, List(a, b))
   
-  val DC = DeclarationContext(List(listDecl), Nil)
+  val vTpeIdA = Id("A")
+  val vTpeIdB = Id("B")
+  val vTpeIdC = Id("C")
+
+  val ctxEmtpyId = Id("ctx_emptyContext")
+  val ctxApplyId = Id("ctx_applyContext")
+  val ctxcomposeId = Id("ctx_composeContext")
+
+  def emptyfun: Extern = Extern.Def(ctxEmtpyId, List(vTpeIdA), Nil, Nil, Nil, HoleContext(ValueType.Var(vTpeIdA), ValueType.Var(vTpeIdA)), Set.empty, StringExternBody(Default(null), null))
+
+  def applyfun: Extern = Extern.Def(ctxApplyId, List(vTpeIdA,vTpeIdB), Nil, List(ValueParam(Id("ctx"),HoleContext(ValueType.Var(vTpeIdA),ValueType.Var(vTpeIdB))),ValueParam(Id("value"),ValueType.Var(vTpeIdB))), Nil, ValueType.Var(vTpeIdA), Set.empty, StringExternBody(Default(null), null))
+
+  def composefun: Extern = Extern.Def(ctxcomposeId, List(vTpeIdA, vTpeIdB, vTpeIdC), Nil, List(ValueParam(Id("ctx1"), HoleContext(ValueType.Var(vTpeIdA), ValueType.Var(vTpeIdB))), ValueParam(Id("ctx2"), HoleContext(ValueType.Var(vTpeIdB),ValueType.Var(vTpeIdC)))), Nil, HoleContext(ValueType.Var(vTpeIdA),ValueType.Var(vTpeIdC)), Set.empty, StringExternBody(Default(null), null))
+
+
+  val DC = DeclarationContext(List(listDecl), List(emptyfun,applyfun,composefun))
 
   val mainSymbol = Id("main")
 
@@ -69,12 +88,11 @@ class TRMCTests extends CoreTests {
     case Compose(first: TailContext, second: TailContext)
   }
   
-  def HoleContext = Type.TInt //(a: ValueType, b: ValueType): ValueType = ValueType.Data(Id("HoleContext"), List(a, b))
-  
-  val composefun: BlockVar = BlockVar(Id("ctx_composeContext"), Function(Nil, Nil, List(HoleContext, HoleContext), Nil, HoleContext), Set.empty) //TODO: where do i get these in the actual compiler?
-    
-  val applyfun: BlockVar = BlockVar(Id("ctx_applyContext"), Function(Nil, Nil, List(HoleContext, HoleContext), Nil, HoleContext), Set.empty) //TODO: types wrong
-  val emptyfun: BlockVar = BlockVar(Id("ctx_emptyContext"), Function(Nil, Nil, Nil, Nil, HoleContext), Set.empty)
+//  def HoleContext = Type.TInt //(a: ValueType, b: ValueType): ValueType = ValueType.Data(Id("HoleContext"), List(a, b))
+//  
+//  val composefun: BlockVar = BlockVar(Id("ctx_composeContext"), Function(Nil, Nil, List(HoleContext, HoleContext), Nil, HoleContext), Set.empty) //TODO: where do i get these in the actual compiler?
+//  val applyfun: BlockVar = BlockVar(Id("ctx_applyContext"), Function(Nil, Nil, List(HoleContext, HoleContext), Nil, HoleContext), Set.empty) //TODO: types wrong
+//  val emptyfun: BlockVar = BlockVar(Id("ctx_emptyContext"), Function(Nil, Nil, Nil, Nil, HoleContext), Set.empty)
 
 
   def split(context: TransformContext): (TailContext, Option[TransformContext]) = context match {
@@ -86,12 +104,12 @@ class TRMCTests extends CoreTests {
     case _ => (TailContext.Empty, Some(context))
   }
   
-  def transform(input: Stmt, inputfun: Id, outputfun: Id, context: TransformContext) : Stmt = input match {
+  def transform(input: Stmt, inputfun: Id, outputfun: Id, outputTpe: ValueType, context: TransformContext, outerContextTpe: ValueType) : Stmt = input match {
     case Stmt.Def(id, block, body) => ???
     case Stmt.Let(id, binding, body) => ???
     case Stmt.ImpureApp(id, callee, targs, vargs, bargs, body) => ???
     case Stmt.Return(expr) => ???
-    case Stmt.Val(id, binding, body) => transform(binding, inputfun, outputfun, TransformContext.Val(id,body,context))
+    case Stmt.Val(id, binding, body) => transform(binding, inputfun, outputfun, outputTpe, TransformContext.Val(id,body,context), outerContextTpe)
     case Stmt.App(callee, targs, vargs, bargs) => callee match {
       case Block.BlockVar(id, annotatedTpe, annotatedCapt) => 
         if (id == inputfun) {
@@ -101,18 +119,18 @@ class TRMCTests extends CoreTests {
               val inner = Stmt.App(
                 Block.BlockVar(
                   outputfun,
-                  Function(tparams, cparams, vparams.appended(HoleContext), bparams, result), annotatedCapt),//TODO:capt? 
+                  Function(tparams, cparams, vparams.appended(outerContextTpe), bparams, result), annotatedCapt),//TODO:capt? 
                 targs,
-                vargs.appended(innerReify(init)),
+                vargs.appended(innerReify(init, outerContextTpe)),
                 bargs)
               rest match {
-                case Some(rest) => reify(inner, rest, inputfun, outputfun)
+                case Some(rest) => reify(inner, rest, inputfun, outputfun, outerContextTpe)
                 case None => inner
               }
             case _ => throw ImpossibleStateError("in an App() Statement a Function should be called")
           }
         }else{
-          reify(input, context, inputfun, outputfun)
+          reify(input, context, inputfun, outputfun, outerContextTpe)
         }
       case Block.BlockLit(tparams, cparams, vparams, bparams, body) => ???
       case Block.Unbox(pure) => ???
@@ -132,20 +150,29 @@ class TRMCTests extends CoreTests {
     case Stmt.Hole(annotatedTpe, span) => ???
   }
   
-  def reify(stmt: Stmt, context: TransformContext, inputfun: Id, outputfun: Id) : Stmt = context match {
+  def reify(stmt: Stmt, context: TransformContext, inputfun: Id, outputfun: Id, outerContextTpe: ValueType) : Stmt = context match {
     case TransformContext.Outer(id) =>  
       val tmpId = Id("tmp")
-      Stmt.Val(tmpId, stmt, Return(PureApp(applyfun, Nil, List(ValueVar(id, HoleContext), ValueVar(tmpId, stmt.tpe))))) //TODO: fix ctxType,tmpType?
+      Stmt.Val(tmpId, stmt, Return(PureApp(blockVarFromExternDef(ctxApplyId), Nil, List(ValueVar(id, outerContextTpe), ValueVar(tmpId, stmt.tpe))))) //TODO: fix ctxType,tmpType?
     case TransformContext.Val(id, body, next) => 
-      Stmt.Val(id, stmt, transform(body, inputfun, outputfun, next))
+      Stmt.Val(id, stmt, transform(body, inputfun, outputfun, body.tpe, next, outerContextTpe))
   }
   
-  def innerReify(context: TailContext): Expr = context match {
-    case TailContext.Empty => PureApp(emptyfun,Nil,Nil)
-    case TailContext.Outer(id) => Expr.ValueVar(id,HoleContext)
+  def innerReify(context: TailContext, outerContextTpe: ValueType): Expr = context match {
+    case TailContext.Empty => PureApp(blockVarFromExternDef(ctxEmtpyId),Nil,Nil)
+    case TailContext.Outer(id) => Expr.ValueVar(id,outerContextTpe)
     case TailContext.Make(data, tag, targs, before, after) => MakeContext(data, tag, targs, before, after)
-    case TailContext.Compose(first, second) => PureApp(composefun, Nil, List(innerReify(first),innerReify(second)))
+    case TailContext.Compose(first, second) => PureApp(blockVarFromExternDef(ctxcomposeId), Nil, List(innerReify(first, outerContextTpe),innerReify(second, outerContextTpe)))
   }
+  
+  def blockVarFromExternDef(id: Id) : Block.BlockVar = {
+    DC.getExternDef(id) match {
+      case Extern.Def(id, tparams, cparams, vparams, bparams, ret, annotatedCapture, body) => BlockVar(id, Function(tparams, cparams, vparams.map(getType), bparams.map(getType), ret), annotatedCapture)
+    }
+  }
+
+  def getType(vparam: ValueParam): ValueType = vparam.tpe
+  def getType(bparam: BlockParam): BlockType = bparam.tpe
   
   
   test("simple function call") {
@@ -154,6 +181,8 @@ class TRMCTests extends CoreTests {
     val f: Id = Id("f")
     val f2: Id = Id("f2")
     val ctx: Id = Id("ctx")
+    val A: Id = Id("A")
+    val ATpe: ValueType = ValueType.Var(A)
     val fType: BlockType = Function(Nil, Nil, List(Type.TInt), Nil, Type.TInt)
     val fCapt: Captures = Set.empty
 
@@ -168,9 +197,9 @@ class TRMCTests extends CoreTests {
     val app = Stmt.App(Block.BlockVar(f, fType, fCapt), Nil, List(Expr.ValueVar(n, Type.TInt)), Nil)
     
     // f2(n,ctx)
-    val appexpected = Stmt.App(Block.BlockVar(f2, Function(Nil, Nil, List(Type.TInt, HoleContext), Nil, Type.TInt), fCapt), Nil, List(Expr.ValueVar(n, Type.TInt),Expr.ValueVar(ctx,HoleContext)), Nil)
+    val appexpected = Stmt.App(Block.BlockVar(f2, Function(Nil, Nil, List(Type.TInt, HoleContext(ATpe,Type.TInt)), Nil, Type.TInt), fCapt), Nil, List(Expr.ValueVar(n, Type.TInt),Expr.ValueVar(ctx,HoleContext(ATpe,Type.TInt))), Nil)
 
-    val apptransformed = transform(app, f, f2, TransformContext.Outer(ctx))
+    val apptransformed = transform(app, f, f2, Type.TInt, TransformContext.Outer(ctx), HoleContext(ATpe,Type.TInt))
 
 //    val input =
 //      """def f(n: Int) =
@@ -192,6 +221,8 @@ class TRMCTests extends CoreTests {
     val f: Id = Id("f")
     val f2: Id = Id("f2")
     val ctx: Id = Id("ctx")
+    val A: Id = Id("A")
+    val ATpe: ValueType = ValueType.Var(A)
     //val listType = ValueType.Var(Id("List"))
     val fType: BlockType = Function(Nil, Nil, List(Type.TInt), Nil, TList)
     val fCapt: Captures = Set.empty
@@ -216,12 +247,12 @@ class TRMCTests extends CoreTests {
     val consctx = Expr.MakeContext(TList,consId,List(Type.TInt,Type.TInt),List(ValueVar(n,Type.TInt)),Nil)
     
     //compose(ctx,Cons(n,[]))
-    val composition = Expr.PureApp(composefun,Nil,List(Expr.ValueVar(ctx,HoleContext), consctx))
+    val composition = Expr.PureApp(blockVarFromExternDef(ctxcomposeId),Nil,List(Expr.ValueVar(ctx,HoleContext(ATpe,TList)), consctx))
     
     //f2(n-1,compose(ctx, Cons(n,[]))
-    val expected = Stmt.App(Block.BlockVar(f2,Function(Nil, Nil, List(Type.TInt, HoleContext), Nil, TList),fCapt),Nil,List(minusOne, composition),Nil)
+    val expected = Stmt.App(Block.BlockVar(f2,Function(Nil, Nil, List(Type.TInt, HoleContext(ATpe,TList)), Nil, TList),fCapt),Nil,List(minusOne, composition),Nil)
     
-    val inputtransformed = transform(inputTree,f,f2,TransformContext.Outer(ctx))
+    val inputtransformed = transform(inputTree,f,f2,TList,TransformContext.Outer(ctx),HoleContext(ATpe,TList))
 
     assertAlphaEquivalentStatements(inputtransformed,expected)
   }
