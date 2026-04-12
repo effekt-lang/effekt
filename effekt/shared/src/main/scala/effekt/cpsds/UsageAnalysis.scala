71420b7bf660c8a76ba93c83f406b89642ebaca8
Jonathan Brachthäuser <jonathan@b-studios.de>
2026-04-12 14:52:23 +0200

    Revise usage analysis a bit

--- effekt/shared/src/main/scala/effekt/cpsds/UsageAnalysis.scala
+++ effekt/shared/src/main/scala/effekt/cpsds/UsageAnalysis.scala
@@ -7,27 +7,24 @@ import scala.collection.mutable
 class FunctionInfo(
   val params: List[Id],
   val body: Stmt,
-  val internalCalls: mutable.ListBuffer[List[Expr]] = mutable.ListBuffer.empty,
+  val recursiveCalls: mutable.ListBuffer[List[Expr]] = mutable.ListBuffer.empty,
   val externalCalls: mutable.ListBuffer[List[Expr]] = mutable.ListBuffer.empty
 ) {
-  def isRecursive: Boolean = internalCalls.nonEmpty
+  def isRecursive: Boolean = recursiveCalls.nonEmpty
 
-  def callCount: Int = internalCalls.size + externalCalls.size
+  def callCount: Int = externalCalls.size
 
-  def isCalledOnce: Boolean = callCount == 1 && !isRecursive
+  def allCalls: Iterable[List[Expr]] = recursiveCalls ++ externalCalls
 
-  def allCalls: Iterable[List[Expr]] = internalCalls ++ externalCalls
+  def staticArguments: List[Boolean] =
+    val calls = recursiveCalls.toVector
 
-  def staticArguments: List[Option[Expr]] =
-    if allCalls.isEmpty then params.map(_ => None)
-    else
-      val all = allCalls.toList
-      val first = all.head
-      first.zipWithIndex.map { case (arg, idx) =>
-        if all.forall(call => call.length > idx && call(idx) == arg)
-        then Some(arg)
-        else None
+    params.zipWithIndex.map {
+      case (param, index) => calls.nonEmpty && calls.map(args => args(index)).forall {
+        case Expr.Variable(other) => param == other
+        case _ => false
       }
+    }
 }
 
 class UsageInfo(
@@ -37,14 +34,16 @@ class UsageInfo(
   def isUsedOnce: Boolean = references == 1
 }
 
-class Analysis(
+class UsageAnalysis(
   val functions: mutable.Map[Id, FunctionInfo] = mutable.Map.empty,
   val usage: mutable.Map[Id, UsageInfo] = mutable.Map.empty,
   var stack: List[Id] = Nil
 ) {
 
   private def use(id: Id): Unit =
-    usage.getOrElseUpdate(id, UsageInfo()).references += 1
+    // Do not count recursive occurrences.
+    if !stack.contains(id) then
+      usage.getOrElseUpdate(id, UsageInfo()).references += 1
 
   def process(e: Expr): Unit = e match {
     case Expr.Variable(id) => use(id)
@@ -79,7 +78,7 @@ class Analysis(
       args.foreach(process)
       functions.get(id).foreach { info =>
         if stack.contains(id) then
-          info.internalCalls += args
+          info.recursiveCalls += args
         else
           info.externalCalls += args
       }
@@ -147,9 +146,9 @@ class Analysis(
     m.definitions.foreach(process)
 }
 
-object Analysis {
-  def apply(m: ModuleDecl): Analysis = {
-    val analysis = new Analysis()
+object UsageAnalysis {
+  def apply(m: ModuleDecl): UsageAnalysis = {
+    val analysis = new UsageAnalysis()
     analysis.process(m)
     analysis
   }
