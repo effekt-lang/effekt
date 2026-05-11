e0e39173c4bfa38c0e0811317bb284d6dc4564fe
Heinrich Timphus <heinrich.timphus@web.de>
2026-05-11 16:42:50 +0200

    rewrite Calls in other functions

--- effekt/shared/src/main/scala/effekt/core/optimizer/TRMC.scala
+++ effekt/shared/src/main/scala/effekt/core/optimizer/TRMC.scala
@@ -4,7 +4,7 @@ import effekt.{Phase, PhaseResult, core, symbols}
 import effekt.PhaseResult.{CoreTransformed, Typechecked}
 import effekt.context.Context
 import effekt.core.Stmt.*
-import effekt.core.*
+import effekt.core.{Toplevel, *}
 import effekt.core.Block.*
 import effekt.core.BlockType.*
 import effekt.symbols.{TermSymbol, TypeSymbol}
@@ -22,16 +22,19 @@ object TRMC extends Phase[CoreTransformed, CoreTransformed]{
     
     var transformedFunctions: List[Toplevel] = List()
     
+    var functionLinks: Map[Id, Id] = Map.empty
+    
     object transform extends Tree.Rewrite {
       override def toplevel: PartialFunction[Toplevel, Toplevel] = {
         case Toplevel.Def(id, block) =>
           println("in Toplevel")
           println(id.name.name)
+          val outputFunId = Id(id.name.name + "_trmc")
           if (id.name.name == "simpleTRMC") {
-            transformedFunctions = transformedFunctions.appended(trmc(id, block, DC))
+            transformedFunctions = transformedFunctions.appended(trmc(id, block, outputFunId, DC))
+            functionLinks = functionLinks+(id -> outputFunId)
           }
-          Toplevel.Def(id, block)//TODO: rewrite calls
-
+          Toplevel.Def(id, block)
       }
 
 //      override def stmt: PartialFunction[Stmt, Stmt] = {
@@ -39,9 +42,22 @@ object TRMC extends Phase[CoreTransformed, CoreTransformed]{
 //          trmc(id, block, body)
 //      }
     }
+    
+    object rewriteOtherCalls extends Tree.Rewrite {
+      override def toplevel: PartialFunction[Toplevel, Toplevel] = {
+        case Toplevel.Def(id, block) => block match {
+          case Block.BlockVar(id, annotatedTpe, annotatedCapt) => ???
+          case Block.BlockLit(tparams, cparams, vparams, bparams, body) =>
+            Toplevel.Def(id, BlockLit(tparams, cparams, vparams, bparams, rewriteCalls(body, id, functionLinks, DC))) 
+          case Block.Unbox(pure) => ???
+          case Block.New(impl) => ???
+        }
+      }
+    }
 
     val transformed = Context.timed(phaseName, source.name) {
-      val m = transform.rewrite(modDec)
+      transform.rewrite(modDec)
+      val m = rewriteOtherCalls.rewrite(modDec)
       m match {
         case ModuleDecl(path, includes, declarations, externs, definitions, exports) =>
           ModuleDecl(path, includes, declarations, externs, definitions ++ transformedFunctions, exports)
@@ -54,8 +70,78 @@ object TRMC extends Phase[CoreTransformed, CoreTransformed]{
   def freeInStmt(id:Id, stmt: Stmt): Boolean = stmt.free.freeIds.contains(id)
   def freeInExpr(id:Id, expr: Expr): Boolean = expr.free.freeIds.contains(id)
   def freeInBlock(id:Id, block: Block): Boolean = block.free.freeIds.contains(id)
-
   
+  def rewriteCalls(stmt: Stmt, transformedfun: Id, functionLinks: Map[Id, Id], DC: DeclarationContext)(using Context): Stmt = stmt match {
+    case Stmt.Def(id, block, body) => ???
+    case Stmt.Let(id, binding, body) => Stmt.Let(id, binding, rewriteCalls(body, transformedfun, functionLinks, DC))
+    case Stmt.ImpureApp(id, callee, targs, vargs, bargs, body) => Stmt.ImpureApp(id, callee, targs, vargs, bargs, rewriteCalls(body, transformedfun, functionLinks, DC))
+    case Stmt.Return(expr) => Stmt.Return(expr)
+    case Stmt.Val(id, binding, body) => Stmt.Val(id, rewriteCalls(binding, transformedfun, functionLinks, DC), rewriteCalls(body, transformedfun, functionLinks, DC))
+    case Stmt.App(callee, targs, vargs, bargs) => callee match {
+      case Block.BlockVar(id, annotatedTpe, annotatedCapt) =>
+        val outputfun = functionLinks.get(id)
+        if(id != transformedfun && outputfun.isDefined){
+          val ctxDecl: Declaration = DC.declarations.find(_.id.name.name == "HoleContext").getOrElse { //TODO: refactor duplicate code
+            Context.panic(s"No declaration found for HoleContext.")
+          }
+          val outerContextTpe = ValueType.Data(ctxDecl.id, List(stmt.tpe, stmt.tpe)) //TODO: parameters, if unequal
+          annotatedTpe match {
+            case Function(tparams, cparams, vparams, bparams, result) =>
+              Stmt.App(
+                Block.BlockVar(
+                  outputfun.get,
+                  Function(tparams, cparams, vparams.appended(outerContextTpe), bparams, result), annotatedCapt),//TODO:capt? 
+                targs,
+                vargs.appended(PureApp(blockVarFromExternDef("ctx_emptyContext", DC), List(stmt.tpe),Nil)),
+                bargs)
+            case _ => throw ImpossibleStateError("in an App() Statement a Function should be called")
+          }
+        }else{
+          stmt
+        }
+      case Block.BlockLit(tparams, cparams, vparams, bparams, body) => ???
+      case Block.Unbox(pure) => ???
+      case Block.New(impl) => ???
+    }
+    case Stmt.Invoke(callee, method, methodTpe, targs, vargs, bargs) => ???
+    case Stmt.If(cond, thn, els) => Stmt.If(cond, rewriteCalls(thn, transformedfun, functionLinks, DC), rewriteCalls(els, transformedfun, functionLinks, DC))
+    case Stmt.Match(scrutinee, annotatedTpe, clauses, default) => 
+      Stmt.Match(
+        scrutinee,
+        annotatedTpe,
+        clauses.map((id, blockLit) => blockLit match {
+          case BlockLit(tparams, cparams, vparams, bparams, body) => (id, BlockLit(tparams, cparams, vparams, bparams, rewriteCalls(body, transformedfun, functionLinks, DC)))
+        }),
+        default match {
+          case Some(value) => Some(rewriteCalls(value, transformedfun, functionLinks, DC))
+          case None => None
+        }
+      )
+    case Stmt.Region(body) => ???
+    case Stmt.Alloc(id, init, region, body) => ???
+    case Stmt.Var(ref, init, capture, body) => ???
+    case Stmt.Get(id, annotatedTpe, ref, annotatedCapt, body) => ???
+    case Stmt.Put(ref, annotatedCapt, value, body) => ???
+    case Stmt.Reset(body) => ???
+    case Stmt.Shift(prompt, k, body) => ???
+    case Stmt.Resume(k, body) => ???
+    case Stmt.Hole(annotatedTpe, span) => ???
+  }
+
+
+  def trmc(id: Id, block: Block, outputfun: Id, DC: DeclarationContext)(using Context): Toplevel = block match {
+    case effekt.core.Block.BlockVar(id, annotatedTpe, annotatedCapt) => ???
+    case effekt.core.Block.BlockLit(tparams, cparams, vparams, bparams, body) =>
+      val ctxDecl: Declaration = DC.declarations.find(_.id.name.name == "HoleContext").getOrElse {
+        Context.panic(s"No declaration found for HoleContext.")
+      }
+      val outerContextTpe = ValueType.Data(ctxDecl.id, List(body.tpe, body.tpe)) //TODO: parameters, if unequal
+      val ctxId = Id("ctx")
+      Toplevel.Def(outputfun, BlockLit(tparams, cparams, vparams.appended(ValueParam(ctxId, outerContextTpe)), bparams,
+        trmc(body, id, outputfun, TransformContext.Outer(ctxId), outerContextTpe, DC)))
+    case effekt.core.Block.Unbox(pure) => ???
+    case effekt.core.Block.New(impl) => ???
+  }
   enum TransformContext {
     case Outer(id: Id)
     case Val(id: Id, body: Stmt, next: TransformContext)
@@ -67,8 +153,6 @@ object TRMC extends Phase[CoreTransformed, CoreTransformed]{
     case Make(data: ValueType.Data, tag: Id, targs: List[ValueType], before: List[Expr], after: List[Expr])
     case Compose(first: TailContext, second: TailContext)
   }
-  
-  
 
   def split(context: TransformContext, DC: DeclarationContext)(using Context): (TailContext, Option[TransformContext]) ={
     val listDecl: Declaration = DC.declarations.find(_.id.name.name == "List").getOrElse {
@@ -90,26 +174,13 @@ object TRMC extends Phase[CoreTransformed, CoreTransformed]{
     
 
 
-  def trmc(id: Id, block: Block, DC: DeclarationContext)(using Context): Toplevel = block match {
-    case effekt.core.Block.BlockVar(id, annotatedTpe, annotatedCapt) => ???
-    case effekt.core.Block.BlockLit(tparams, cparams, vparams, bparams, body) => 
-      val outputFunId = Id(id.name.name + "_trmc")
-      val ctxDecl: Declaration = DC.declarations.find(_.id.name.name == "HoleContext").getOrElse {
-        Context.panic(s"No declaration found for HoleContext.")
-      }
-      val outerContextTpe = ValueType.Data(ctxDecl.id,List(body.tpe, body.tpe))
-      val ctxId = Id("ctx")
-      Toplevel.Def(outputFunId, BlockLit(tparams, cparams, vparams.appended(ValueParam(ctxId, outerContextTpe)), bparams,
-        trmc(body, id, outputFunId, TransformContext.Outer(ctxId), outerContextTpe, DC))) //TODO: fix HoleContext (its in the Declerations, but how to parametrize?)
-    case effekt.core.Block.Unbox(pure) => ???
-    case effekt.core.Block.New(impl) => ???
-  }
   
-  def trmc(input: Stmt, inputfun: Id, outputfun: Id, context: TransformContext, outerContextTpe: ValueType, DC: DeclarationContext)(using Context): Stmt = input match {
+  
+  def trmc(stmt: Stmt, inputfun: Id, outputfun: Id, context: TransformContext, outerContextTpe: ValueType, DC: DeclarationContext)(using Context): Stmt = stmt match {
     case Stmt.Def(id, block, body) => ???
     case Stmt.Let(id, binding, body) => Stmt.Let(id, binding, trmc(body,inputfun, outputfun, context, outerContextTpe, DC))
     case Stmt.ImpureApp(id, callee, targs, vargs, bargs, body) => ???
-    case Stmt.Return(expr) => reify(input, context, inputfun, outputfun, outerContextTpe, DC) //probably works every time, original function must still exit in case inputfun is free in expr
+    case Stmt.Return(expr) => reify(stmt, context, inputfun, outputfun, outerContextTpe, DC) //probably works every time, original function must still exit in case inputfun is free in expr
     case Stmt.Val(id, binding, body) => trmc(binding, inputfun, outputfun, TransformContext.Val(id,body,context), outerContextTpe, DC)
     case Stmt.App(callee, targs, vargs, bargs) => callee match {
       case Block.BlockVar(id, annotatedTpe, annotatedCapt) =>
@@ -122,7 +193,7 @@ object TRMC extends Phase[CoreTransformed, CoreTransformed]{
                   outputfun,
                   Function(tparams, cparams, vparams.appended(outerContextTpe), bparams, result), annotatedCapt),//TODO:capt? 
                 targs,
-                vargs.appended(innerReify(init, outerContextTpe, input.tpe, DC)), //is input.tpe always the correct resType?
+                vargs.appended(innerReify(init, outerContextTpe, stmt.tpe, DC)), //is input.tpe always the correct resType?
                 bargs)
               rest match {
                 case Some(rest) => reify(inner, rest, inputfun, outputfun, outerContextTpe, DC)
@@ -131,7 +202,7 @@ object TRMC extends Phase[CoreTransformed, CoreTransformed]{
             case _ => throw ImpossibleStateError("in an App() Statement a Function should be called")
           }
         }else{
-          reify(input, context, inputfun, outputfun, outerContextTpe, DC)
+          reify(stmt, context, inputfun, outputfun, outerContextTpe, DC)
         }
       case Block.BlockLit(tparams, cparams, vparams, bparams, body) => ???
       case Block.Unbox(pure) => ???
