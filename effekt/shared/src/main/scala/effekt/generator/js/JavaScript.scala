13872ea510007232c0595b0e8776d802702ebfe5
Jonathan Brachthäuser <jonathan@b-studios.de>
2026-08-07 18:29:09 +0200

    Implement more general parameter dropping

--- effekt/shared/src/main/scala/effekt/generator/js/JavaScript.scala
+++ effekt/shared/src/main/scala/effekt/generator/js/JavaScript.scala
@@ -67,7 +67,7 @@ class JavaScript(additionalFeatureFlags: List[String] = Nil) extends Compiler[St
         tree = effekt.cpsds.BlockSinking.transform(tree, mainId)
         println("--- AFTER ---")
         println(util.show(tree))
-        //tree = effekt.cpsds.ParameterDropping.transform(tree, mainId)
+        //tree = effekt.cpsds.ParameterDropping.transform(tree)
         //tree = effekt.cpsds.Simplifier.transform(tree)
         tree
 
