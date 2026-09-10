b34f5ce04464d753960f4debb98c5beda1055497
Jiří Beneš <mail@jiribenes.com>
2026-09-10 14:50:28 +0200

    More generous timeouts to account for slow CI / computers

--- effekt/jvm/src/test/scala/effekt/LLVMTests.scala
+++ effekt/jvm/src/test/scala/effekt/LLVMTests.scala
@@ -7,7 +7,7 @@ import scala.language.implicitConversions
 import scala.sys.process.Process
 
 class LLVMTests extends EffektTests {
-  override val munitTimeout = scala.concurrent.duration.Duration(60, "s")
+  override val munitTimeout = scala.concurrent.duration.Duration(5, "m")
 
   def backendName = "llvm"
 
