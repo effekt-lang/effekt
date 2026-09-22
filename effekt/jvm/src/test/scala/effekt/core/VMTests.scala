693c6b3495e748b707ea36a008e69ebe436ce66a
Jiří Beneš <mail@jiribenes.com>
2026-09-22 15:36:47 +0200

    Add generator and handler_sieve to VMTests

--- effekt/jvm/src/test/scala/effekt/core/VMTests.scala
+++ effekt/jvm/src/test/scala/effekt/core/VMTests.scala
@@ -511,6 +511,38 @@ class VMTests extends munit.FunSuite {
       resumes = 0
     )),
 
+    examplesDir / "benchmarks" / "effect_handlers_bench" / "generator.effekt" -> Some(Summary(
+      staticDispatches = 101,
+      dynamicDispatches = 31,
+      patternMatches = 95,
+      branches = 6,
+      pushedFrames = 102,
+      poppedFrames = 102,
+      allocations = 38,
+      closures = 31,
+      variableReads = 0,
+      variableWrites = 0,
+      resets = 1,
+      shifts = 31,
+      resumes = 31
+    )),
+
+    examplesDir / "benchmarks" / "effect_handlers_bench" / "handler_sieve.effekt" -> Some(Summary(
+      staticDispatches = 9,
+      dynamicDispatches = 21,
+      patternMatches = 0,
+      branches = 34,
+      pushedFrames = 9,
+      poppedFrames = 9,
+      allocations = 0,
+      closures = 5,
+      variableReads = 0,
+      variableWrites = 0,
+      resets = 0,
+      shifts = 0,
+      resumes = 0
+    )),
+
     examplesDir / "benchmarks" / "effect_handlers_bench" / "iterator.effekt" -> Some(Summary(
       staticDispatches = 7,
       dynamicDispatches = 0,
